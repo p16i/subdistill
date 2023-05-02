b26cf2b25a0b1382277bda55082b46b4b7af64bf
Pat Chormai <pat.chormai@gmail.com>
2023-05-02 12:05:00 +0200

    add sbatch job

---
+++ slurm/job_2h.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -p gpu-2h
+#SBATCH --gpus=1
+#SBATCH -o ./logs/%j.out
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=p.chormai@tu-berlin.de
+
+nvidia-smi
+
+poetry run "$@"
