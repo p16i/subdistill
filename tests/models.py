0b0439701d3e562ef1f76a00a6aa227e3532d046
Pat Chormai <pat.chormai@gmail.com>
2023-05-15 23:36:39 +0200

    add split model

--- tests/models.py
+++ tests/models.py
@@ -1,4 +1,6 @@
+import torch
 import pytest
+import numpy as np
 
 from xaikd import models
 
@@ -10,3 +12,25 @@ def test_get_models(slug):
     model = models.get_model(slug)
     assert not model.training
     assert models.get_model(slug) is not None
+
+
+@pytest.mark.parametrize(
+    "slug", ["cifar10-resnet18-p1", "cifar100-resnet18-p1", "imagenet-resnet18-tv"]
+)
+@pytest.mark.parametrize("layer", ["layer1", "layer2", "layer3", "layer4"])
+@pytest.mark.slow
+def test_split_model(slug, layer):
+    model = models.get_model(slug)
+
+    head, classifier = models.resnet.spit_resnet_18_at(model, layer)
+
+    if "imagenet" in slug:
+        input = torch.randn(10, 3, 224, 224)
+    else:
+        input = torch.randn(10, 3, 32, 32)
+
+    with torch.no_grad():
+        actual = classifier(head(input)).numpy()
+        expected = model(input).numpy()
+
+        np.testing.assert_allclose(actual, expected)
