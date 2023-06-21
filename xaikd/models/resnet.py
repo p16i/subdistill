3d5206b757030549ab2e508bbc7e38630a67abb3
Pat Chormai <pat.chormai@gmail.com>
2023-06-21 10:20:38 +0200

    improve model spliting

--- xaikd/models/resnet.py
+++ xaikd/models/resnet.py
@@ -18,18 +18,21 @@ ARCH_LAYER_DIMENSIONS = dict(
 def split_resnet_18_at(
     model: nn.Module, layer: str
 ) -> typing.Tuple[nn.Module, nn.Module, nn.Module]:
-    layer_ix = int(layer[-1])
+    layer_ix = int(layer[-1]) - 1
 
     layers = [model.layer1, model.layer2, model.layer3, model.layer4]
 
+    layers_in_head = layers[:layer_ix] if layer_ix > 0 else []
+    layers_in_classifier = layers[layer_ix + 1 :]
+
     head = nn.Sequential(
-        model.conv1, model.bn1, model.relu, model.maxpool, *layers[: layer_ix - 1]
+        model.conv1, model.bn1, model.relu, model.maxpool, *layers_in_head
     )
 
     layer_module = layers[layer_ix]
 
     classifier = nn.Sequential(
-        *layers[layer_ix:], model.avgpool, nn.Flatten(start_dim=1), model.fc
+        *layers_in_classifier, model.avgpool, nn.Flatten(start_dim=1), model.fc
     )
 
     return head, layer_module, classifier
