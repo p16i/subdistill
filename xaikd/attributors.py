9f6ba8b4a43110edaa16c588eabaa00ba5d48c6b
Pat Chormai <pat.chormai@gmail.com>
2023-06-30 15:19:59 +0200

    add extract activation

--- xaikd/attributors.py
+++ xaikd/attributors.py
@@ -61,7 +61,9 @@ class OneClassLogSumExpEvidence(LogitModifier):
 
     def __call__(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         logits = logits.clone()
-        logexp = torch.logsumexp(logits[:, self.dataset.selected_classes], dim=1, keepdim=True)
+        logexp = torch.logsumexp(
+            logits[:, self.dataset.selected_classes], dim=1, keepdim=True
+        )
         return (logits - logexp) * F.one_hot(targets, self.dataset.num_classes).to(
             logits.device
         )
@@ -152,3 +154,43 @@ def extract_activation_context(
     arr_ctx = np.vstack(arr_ctx)
 
     return arr_act, arr_ctx
+
+
+def extract_activation(
+    model: nn.Module,
+    layer: str,
+    dataset: datasets.DatasetConfiguration,
+    data_loader: DataLoader,
+    device="cpu",
+    number_of_selected_spatial_locations=20,
+) -> typing.Tuple[npt.NDArray, npt.NDArray]:
+    arr_act = []
+    arr_ctx = []
+
+    try:
+        module, hook = utils.interceptor.attach_hook_intercept_layer_output(
+            model, layer
+        )
+
+        with make_attributor_for(model, dataset.input_statistics) as attributor:
+            for batch in tqdm(data_loader):
+                x, y = batch
+                x = x.to(device)
+
+                _ = model(x)
+
+                act = utils.interceptor.get_output(module)
+
+                act = act.detach().cpu().numpy()
+
+                selected_act, _ = utils.subsample_tensors(
+                    act, act, num_locations=number_of_selected_spatial_locations
+                )
+                arr_act.append(selected_act)
+
+    finally:
+        hook.remove()
+
+    arr_act = np.vstack(arr_act)
+
+    return arr_act
