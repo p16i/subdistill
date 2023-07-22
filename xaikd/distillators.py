e10e4f93b85bf47d097cd15d0555320d6d2f5251
Pat Chormai <pat.chormai@gmail.com>
2023-07-22 09:38:26 +0200

    add 1x1 convolution for approximate module

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -177,13 +177,20 @@ def get_approximator_for_resnet18(
 
     blocks = len(getattr(model, layer))
 
-    return model._make_layer(
-        torchvision.models.resnet.BasicBlock,
-        output_dimensions,
-        blocks,
-        stride=2,
-        dilate=False,
+    output = nn.Sequential(
+        model._make_layer(
+            torchvision.models.resnet.BasicBlock,
+            output_dimensions,
+            blocks,
+            stride=2,
+            dilate=False,
+        ),
+        nn.Conv2d(
+            in_channels=output_dimensions, out_channels=output_dimensions, kernel_size=1
+        ),
     )
+
+    return output
 
 
 class Layerwise:
