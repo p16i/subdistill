b44c0583cf8be1204806a1cbc489cb06938947d1
Pat Chormai <pat.chormai@gmail.com>
2023-07-29 20:04:39 +0200

    add pcaprca bases

--- scripts/distill-v2.py
+++ scripts/distill-v2.py
@@ -40,7 +40,7 @@ def get_transformation(dataset_name):
 @click.option(
     "--basis-names",
     type=str,
-    default="pca,prca-recon,prca-abs,random1,random2",
+    default="pca,prca-recon,prca-abs,pcaprca-abs,pcaprca-recon,random1,random2",
     required=True,
 )
 @click.option("--basis-mode", type=str, default="centered", required=True)
