a24c925bb3f9211708a7becce4b1e8d7d8c05c40
Pat Chormai <pat.chormai@gmail.com>
2023-07-31 09:42:07 +0200

    add mnist-demo k28

--- xaikd/bases/__init__.py
+++ xaikd/bases/__init__.py
@@ -220,7 +220,11 @@ class PCA(Basis):
         if self.centering:
             activation = activation - mean
 
-        eigvals, eigvecs = np.linalg.eigh(activation.T @ activation / n)
+        assert not np.isnan(activation).any()
+
+        cov = activation.T @ activation / n
+
+        eigvals, eigvecs = np.linalg.eigh(cov)
 
         indices = np.argsort(eigvals)[::-1]
 
@@ -229,6 +233,17 @@ class PCA(Basis):
 
         std = np.std(activation @ eigvecs, axis=0)
 
+        cond = eigvals < 0
+
+        if cond.sum() > 0:
+            print("[warning]: some eigenvalues are of PCA smaller than zero!")
+            print(
+                "Because this seems to be numerical issue, we set eigvals[eigvals < 0] = 0"
+            )
+            eigvals[cond] = 0
+
+        assert not np.isnan(std).any()
+        assert not (eigvals < 0).any()
         np.testing.assert_allclose(std, eigvals**0.5, atol=1e-3)
 
         self.artifact = dict(zip(self.artifact_keys, (eigvecs, std)))
