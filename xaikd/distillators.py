28a4ad152428dff9d4f530df22bdea008a7a04c8
Pat Chormai <pat.chormai@gmail.com>
2023-08-04 23:55:22 +0200

    remove scaling

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -185,7 +185,8 @@ class ModelWrapper(pl.LightningModule):
                 )
 
         loss_mse = F.mse_loss(feat_out, expected_out, reduction="none")
-        loss_mse = loss_mse.flatten(start_dim=1) / (h * w)
+        loss_mse = loss_mse.flatten(start_dim=1)
+        # / (h * w)
 
         loss_mse = loss_mse.sum(dim=1)
 
