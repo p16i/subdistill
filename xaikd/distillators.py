97337adfba23014fec8e3516bfb07941d6c65ce2
Pat Chormai <pat.chormai@gmail.com>
2023-08-04 14:49:15 +0200

    Revert "add hidge loss"
    This reverts commit 35255de3edfb23e7f20996dc38a5bd4ae852823a.

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -151,7 +151,6 @@ class ModelWrapper(pl.LightningModule):
         _, _, h, w = expected_out.shape
 
         loss_mse = F.mse_loss(feat_out, expected_out, reduction="none")
-        loss_mse = F.relu(loss_mse - 0.1)
         loss_mse = loss_mse.flatten(start_dim=1) / (h * w)
         loss_mse = loss_mse.sum(dim=1)
 
