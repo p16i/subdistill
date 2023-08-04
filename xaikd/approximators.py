562a603282e703db927e71523603a300fbe8cbe1
Pat Chormai <pat.chormai@gmail.com>
2023-08-05 00:31:35 +0200

    add batch norm

--- xaikd/approximators.py
+++ xaikd/approximators.py
@@ -42,7 +42,17 @@ def construct_approximator_for(
     elif mode == ApproximatorMode.HOMOGENOUS_LOWRANK_ADAPTER:
         last_module = nn.Conv2d(in_channels=k, out_channels=d, kernel_size=1)
     elif mode == ApproximatorMode.HOMOGENOUS_LOWRANK:
-        last_module = nn.Conv2d(in_channels=k, out_channels=k, kernel_size=1)
+        last_module = nn.Sequential(
+            nn.Conv2d(
+                in_channels=k,
+                out_channels=k,
+                kernel_size=1,
+            ),
+            nn.BatchNorm2d(
+                num_features=k,
+                affine=False,
+            ),
+        )
 
     return nn.Sequential(backbone_approximator, last_module)
 
