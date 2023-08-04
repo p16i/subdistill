43c4c1de8fadd97a6b21cbdfef50847c88b08cb7
Pat Chormai <pat.chormai@gmail.com>
2023-08-04 17:38:47 +0200

    remove scheduler

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -88,8 +88,8 @@ class ModelWrapper(pl.LightningModule):
         optimizer = torch.optim.Adam(
             self.approximator.parameters(), lr=self.lr, weight_decay=self.weight_decay
         )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.5)
-        return [optimizer], [scheduler]
+        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.5)
+        # return [optimizer], [scheduler]
         return optimizer
 
     def forward_with_feats(
