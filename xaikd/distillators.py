1845549881f653f136220e4005219c6200531664
Pat Chormai <pat.chormai@gmail.com>
2023-08-05 08:36:11 +0200

    remove scaling

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -155,9 +155,6 @@ class ModelWrapper(pl.LightningModule):
 
         _, _, h, w = expected_out.shape
 
-        feat_out = feat_out
-        # * self.adapter.std
-
         with torch.no_grad():
             if batch_idx == 0 and prefix == "val":
                 print(
