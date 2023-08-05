4e6bf505e732ee9caaf06ffbb755c786482245f1
Pat Chormai <pat.chormai@gmail.com>
2023-08-05 02:30:45 +0200

    try step25-gamma0.5

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -88,7 +88,7 @@ class ModelWrapper(pl.LightningModule):
         optimizer = torch.optim.Adam(
             self.approximator.parameters(), lr=self.lr, weight_decay=self.weight_decay
         )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.5)
         return [optimizer], [scheduler]
 
     def forward_with_feats(
