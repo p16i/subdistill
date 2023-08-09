c2a6fdf7825b82d22c839080f903f0cb4cae1246
Pat Chormai <pat.chormai@gmail.com>
2023-08-09 17:32:06 +0200

    add assert

--- xaikd/bases/__init__.py
+++ xaikd/bases/__init__.py
@@ -423,6 +423,14 @@ class RelReconGreedy(CanonicalBasis):
 
             rel_total_left = (a_comp * c_comp).sum(axis=1)
 
+            assert np.testing.assert_allclose(
+                rel_total_left.cpu().numpy(),
+                (activation[:, dimensions] * context[:, dimensions])
+                .sum(axis=1)
+                .cpu()
+                .numpy(),
+            )
+
             for i in dimensions:
                 u = torch.zeros(d)
                 u[i] = 1
