58ed75e4b954e7ce77069dcc2ba1782443640966
Pat Chormai <pat.chormai@gmail.com>
2023-08-15 16:42:31 +0200

    subclasses dataset

--- scripts/model_accuracy.py
+++ scripts/model_accuracy.py
@@ -17,18 +17,26 @@ import pandas as pd
 from tqdm import tqdm
 
 
+import typing
+
+
 def compute_xent_and_accuracy(
-    model: nn.Module, dl: DataLoader, num_classes=100, device="cpu"
+    model: nn.Module,
+    dataset,
+    dl: DataLoader,
+    device="cpu",
 ) -> typing.Tuple[float, float]:
     # todo: move this to `utils`
     model.eval()
-    metric = Accuracy(task="multiclass", num_classes=num_classes)
+    metric = Accuracy(task="multiclass", num_classes=dataset.num_classes)
     metric_xent = MeanMetric()
     for x, y in tqdm(dl):
         logits = model(x.to(device)).cpu()
+        selected_logits = logits[:, dataset.selected_classes]
+        selected_y = dataset.transform_target(y)
 
-        metric_xent.update(F.cross_entropy(logits, y))
-        metric.update(logits, y)
+        metric_xent.update(F.cross_entropy(selected_logits, selected_y))
+        metric.update(selected_logits selected_y)
 
     acc = float(metric.compute())
     xent = float(metric_xent.compute())
@@ -75,7 +83,7 @@ def main(dataset_name, model_names, output_dir):
         print(f"Model={model_name}")
         model_stat = dict(model=model_name)
         for prefix, dl in [("train", train_dl), ("val", val_dl)]:
-            xent, acc = compute_xent_and_accuracy(model, dl, device=device)
+            xent, acc = compute_xent_and_accuracy(model, dataset, dl, device=device)
             print(f" > [{prefix:5s}] xent={xent:.4f} acc={acc:.4f}")
 
             model_stat[f"{prefix}_xent"] = xent
