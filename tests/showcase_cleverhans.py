f0c6a1d6e19d5338826eca354a9d1942202ff741
Pat Chormai <pat.chormai@gmail.com>
2023-08-20 10:58:23 +0200

    showcase 1: contaminate `val_ds` also

--- tests/showcase_cleverhans.py
+++ tests/showcase_cleverhans.py
@@ -9,29 +9,41 @@ from xaikd.showcases import cleverhans
 
 @pytest.mark.parametrize("contamination_level", [0.0, 0.1, 0.3])
 @pytest.mark.parametrize("training_size", [0.1, 0.5])
-@pytest.mark.parametrize("dataset_name", ["cifar100-people", "cifar100"])
-def test_contamination_dataset(dataset_name, contamination_level, training_size):
+@pytest.mark.parametrize(
+    "dataset_name,victim_class_indices",
+    [
+        ("cifar100-people", [2]),
+        ("cifar100-people", [2, 46]),
+        ("cifar100-people", [2, 11, 35, 46, 98]),
+        ("cifar100", [0]),
+        ("cifar100", list(range(10))),
+    ],
+)
+def test_contamination_dataset(
+    dataset_name, victim_class_indices, contamination_level, training_size
+):
     dataset = datasets.construct(dataset_name)
 
     ds_clean = datasets.subsample_dataset(
         dataset.create_subset(train_split=True), ratio=training_size, seed=1
     )
 
+    targets = np.array(ds_clean.dataset.targets)
+
     ds_contaminated = cleverhans.contaminate_dataset(
-        ds_clean, contamination_level=contamination_level, seed=3
+        ds_clean,
+        contamination_level=contamination_level,
+        seed=3,
+        victim_class_indices=victim_class_indices,
     )
 
     subsampled_indices = ds_clean.indices
-    targets = np.array(ds_clean.dataset.targets)
 
     assert id(ds_contaminated.dataset) != id(ds_clean.dataset)
     np.testing.assert_equal(ds_contaminated.indices, ds_clean.indices)
 
-    # remark: this convention is used in the function.
-    victim_class_ix = np.min(targets)
-
     all_possible_victim_samples_indices = np.argwhere(
-        targets == victim_class_ix
+        np.isin(targets, victim_class_indices)
     ).reshape(-1)
 
     potential_victim_samples_indices = np.array(
@@ -44,6 +56,11 @@ def test_contamination_dataset(dataset_name, contamination_level, training_size)
         (potential_victim_samples_indices, contamination_level),
         (non_victim_samples_indices, 0.0),
     ]:
+        if len(index_set) == expected_ratio == 0:
+            # this is the case that have contaminate all classes in the dataset
+            # therefore, we do NOT have non victim samples.
+            continue
+
         clean_data: npt.NDArray = ds_clean.dataset.data[index_set, :]
         contaminated_data: npt.NDArray = ds_contaminated.dataset.data[index_set, :]
         assert clean_data.shape[0] == contaminated_data.shape[0]
