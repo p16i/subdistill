98804f3589a17f0139887ddd39fafdb3b579cee4
Pat Chormai <pat.chormai@gmail.com>
2023-08-24 15:04:46 +0200

    add batchnorm

--- xaikd/approximators.py
+++ xaikd/approximators.py
@@ -66,19 +66,20 @@ def construct_approximator_for(
             compression_ratio == 1.0
         ), f"`{mode}` only work with `compression_rate=1.0`"
 
-        last_module = nn.Identity()
+        last_module = nn.BatchNorm2d(num_features=k)
     elif mode == ApproximatorMode.HOMOGENOUS_LOWRANK_ADAPTER:
         # todo:
-        raise NotImplementedError("Conv2D has to output only positive weight!")
+        print("[warning]: Conv2D has to output only positive weight!")
         last_module = nn.Sequential(
             nn.Conv2d(in_channels=k, out_channels=d, kernel_size=1),
             # remark: this is very specific to what the range of the original module
             nn.Identity(),
         )
     elif mode == ApproximatorMode.HOMOGENOUS_LOWRANK:
-        last_module = nn.Conv2d(
-            in_channels=k, out_channels=k, kernel_size=1, bias=False
-        )
+        # last_module = nn.Conv2d(
+        #     in_channels=k, out_channels=k, kernel_size=1, bias=False
+        # )
+        last_module = nn.BatchNorm2d(num_features=k)
 
     return nn.Sequential(backbone_approximator, last_module)
 
