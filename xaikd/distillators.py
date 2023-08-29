293cf33eb45c0b9630b2e6742bb923f71dcab60e
Pat Chormai <pat.chormai@gmail.com>
2023-08-29 20:11:28 +0200

    update variable

--- xaikd/distillators.py
+++ xaikd/distillators.py
@@ -165,7 +165,7 @@ class ModelWrapper(pl.LightningModule):
         with torch.no_grad():
             expected_out = self.teacher_module(feat_in)
 
-        _, d, w, h = expected_out.shape
+        _, _, w, h = expected_out.shape
 
         loss_mse = F.mse_loss(feat_out, expected_out, reduction="none") / (w * h)
         loss_mse = loss_mse.flatten(start_dim=1)
