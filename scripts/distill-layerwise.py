e2d503cd58d6d49272882c03e43c5d8b59cf6b28
Pat Chormai <pat.chormai@gmail.com>
2023-09-17 15:39:10 +0200

    add more policies

--- scripts/distill-layerwise.py
+++ scripts/distill-layerwise.py
@@ -202,6 +202,7 @@ def main(
                 "basis-student-identity",
                 "basis-student-ortho",
                 "basis-student-orthoinner",
+                "basis-student-orthoinnerstudentnorm",
                 "basis-student-orthoconstinner",
                 "basis-student-orthoconstinnerstudentnorm",
             ]:
