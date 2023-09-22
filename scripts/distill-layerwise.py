390586c73328b6540066c79071906031ca15959d
Pat Chormai <pat.chormai@gmail.com>
2023-09-22 17:54:19 +0200

    use new wand project

--- scripts/distill-layerwise.py
+++ scripts/distill-layerwise.py
@@ -35,7 +35,7 @@ from xaikd.utils import click_types
 from pytorch_lightning.loggers import WandbLogger
 
 
-WANDB_PROJECT = os.getenv("WANDB_PROJECT", "xaikd-distillation-layerwise")
+WANDB_PROJECT = os.getenv("WANDB_PROJECT", "xaikd-distillation-layerwise-ep2")
 
 
 def learn_basese(
