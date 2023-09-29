f1b7fa1ac3ff733dca8e45a292e8a29450af263e
Pat Chormai <pat.chormai@gmail.com>
2023-09-29 07:38:24 +0200

    add customized student

--- xaikd/models/resnet.py
+++ xaikd/models/resnet.py
@@ -174,6 +174,88 @@ def _generate_resnet18_compressed(
     return model
 
 
+@register_model("resnet18cifarcustomized")
+def _generate_cifarresnet18_customized(num_classes, for_cifar=True) -> nn.Module:
+    # todo: hard-corded everything for now.
+    resnet18 = torchvision.models.resnet.resnet18()
+
+    # ref: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L184
+
+    inplanes = 32
+    # becuase inplance is modified throught the generation
+    # we have to reset attribute
+    resnet18.inplanes = inplanes
+
+    layers = []
+
+    if for_cifar:
+        # ref: https://github.com/lightly-ai/lightly/blob/b69b8b14c29121422479f23078488efca734a995/lightly/models/resnet.py#L4
+        layers.extend(
+            [
+                # ref: https://github.com/lightly-ai/lightly/blob/b69b8b14c29121422479f23078488efca734a995/lightly/models/resnet.py#L193
+                (
+                    "conv1",
+                    nn.Conv2d(
+                        3, inplanes, kernel_size=3, padding=1, stride=1, bias=False
+                    ),
+                ),
+                ("bn1", nn.BatchNorm2d(num_features=inplanes)),
+                ("relu1", nn.ReLU()),
+                ("maxpool", nn.Identity()),
+            ]
+        )
+    else:
+        layers.extend(
+            [
+                # https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L197
+                (
+                    "conv1",
+                    nn.Conv2d(
+                        3, inplanes, kernel_size=7, padding=3, stride=2, bias=False
+                    ),
+                ),
+                ("bn1", nn.BatchNorm2d(num_features=inplanes)),
+                ("relu1", nn.ReLU()),
+                ("maxpool", nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+            ]
+        )
+
+    arr_num_blocks = [2, 2, 2, 2]
+    arr_dims = [32, 64, 10, 5]
+
+    for i, (dims, num_blocks) in enumerate(zip(arr_dims, arr_num_blocks)):
+        layer = resnet18._make_layer(
+            torchvision.models.resnet.BasicBlock,
+            dims,
+            num_blocks,
+            # ref: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L202
+            stride=2 if i > 0 else 1,
+            # ref: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py#L78
+            dilate=False,
+        )
+
+        # # todo: this is temporary;
+        # todo: remove this after also in other derivative of the architecture.
+        layers.append(
+            (
+                f"layer{i+1}",
+                nn.Sequential(layer, nn.BatchNorm2d(num_features=dims)),
+            )
+        )
+
+    layers.extend(
+        [
+            ("avgpool", nn.AdaptiveAvgPool2d((1, 1))),
+            ("flatten", nn.Flatten(start_dim=1)),
+            ("fc", nn.Linear(in_features=arr_dims[-1], out_features=num_classes)),
+        ]
+    )
+
+    model = nn.Sequential(OrderedDict(layers))
+
+    return model
+
+
 @register_model("resnet18cifarcompr1")
 def _cifarresnet18c1(num_classes: int):
     return _generate_resnet18_compressed(
