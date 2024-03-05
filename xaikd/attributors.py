c33de2ba3d4d108bc4dc2894ecdbcb5d5d66f3b3
Pat Chormai <pat.chormai@gmail.com>
2024-03-05 18:37:56 +0100

    add inv logit

--- xaikd/attributors.py
+++ xaikd/attributors.py
@@ -76,6 +76,21 @@ class WinningClassEvidence(LogitModifier):
         return "winingclass"
 
 
+class WinningClassInvLogitEvidence(LogitModifier):
+    def __init__(self, num_classes: int) -> None:
+        self.num_classes = num_classes
+
+    def __call__(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        logits = logits.clone()
+        wining_targets = torch.argmax(logits, dim=1)
+        return (1 / logits) * F.one_hot(wining_targets, self.num_classes).to(
+            logits.device
+        )
+
+    def __str__(self) -> str:
+        return "winingclass"
+
+
 class WinningClassOneHotEvidence(LogitModifier):
     def __init__(self, num_classes: int) -> None:
         self.num_classes = num_classes
