8286e8bec9e11e40ec736212d292e2c5849302ee
Pat Chormai <pat.chormai@gmail.com>
2024-03-08 09:52:35 +0100

    merge test

--- tests/models.py
+++ tests/models.py
@@ -29,38 +29,9 @@ def test_get_models(slug):
     assert getattr(model, "__name") == slug
     assert len(getattr(model, "__layer_dimension").keys()) > 0
 
-    dataset = slug.split("-")[0]
-    if dataset == "cifar100":
-        data = torch.rand(5, 3, 32, 32)
-    elif dataset == "imagenet":
-        data = torch.rand(5, 3, 224, 224)
-
-    output = model(data)
-
-
-@pytest.mark.parametrize(
-    "slug",
-    [
-        "cifar100-resnet18-v1",
-        "imagenet-resnet18-tv",
-        "imagenet-resnet34-tv",
-        "imagenet-resnet50-tv",
-        "imagenet-resnet101-tv",
-        "imagenet-resnet152-tv",
-        "imagenet-vgg11-tv",
-        "imagenet-vgg16-tv",
-        "imagenet-vgg16bn-tv",
-    ],
-)
-@pytest.mark.slow
-@torch.no_grad()
-def test_verify_layer_dimensions(slug):
-    torch.manual_seed(1)
-    model = models.get_trained_model(slug)
-
     device = utils.get_device()
 
-    model.to(device)
+    model = model.to(device)
 
     dataset, arch, variant = slug.split("-")
     if dataset == "cifar100":
@@ -68,8 +39,12 @@ def test_verify_layer_dimensions(slug):
     elif dataset == "imagenet":
         data = torch.rand(5, 3, 224, 224)
 
-    data.to(device)
+    data = data.to(device)
 
+    # model is forwardable
+    _ = model(data)
+
+    # verify that the dimension mapping is correct
     for layer, expected_dims in constants.ARCH_LAYER_DIMENSIONS[arch].items():
         try:
             module, hook = utils.interceptor.attach_hook_intercept_layer_output(
