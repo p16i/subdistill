94a322e7ee5a61ad31885b5de9deba4c18c14286
Pat Chormai <pat.chormai@gmail.com>
2024-03-11 07:28:26 +0100

    use gpu

--- tests/model_resnet.py
+++ tests/model_resnet.py
@@ -3,7 +3,7 @@ import pytest
 import torch
 import numpy as np
 
-from xaikd import models
+from xaikd import models, utils
 
 
 @pytest.mark.parametrize(
@@ -20,16 +20,20 @@ from xaikd import models
 @pytest.mark.parametrize("layer", ["layer1", "layer2", "layer3", "layer4"])
 @pytest.mark.slow
 def test_split_resnet_model(slug, layer):
+    device = utils.get_device()
+
     model = models.get_trained_model(slug)
+    model.to(device)
 
     head, classifier = models.resnet.split_model_at(model, layer)
     if "imagenet" in slug:
         input = torch.randn(10, 3, 224, 224)
     else:
         input = torch.randn(10, 3, 32, 32)
+    input = input.to(device)
 
     with torch.no_grad():
-        actual = classifier(head(input)).numpy()
-        expected = model(input).numpy()
+        actual = classifier(head(input)).cpu().numpy()
+        expected = model(input).cpu().numpy()
 
         np.testing.assert_allclose(actual, expected)
