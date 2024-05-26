f6f8566c8913342db165f5a4d680641e80fc7e5b
Pat Chormai <pat.chormai@gmail.com>
2024-05-26 20:22:21 +0200

    add nfnet

--- xaikd/constants.py
+++ xaikd/constants.py
@@ -10,81 +10,81 @@ ARR_STUDENT_DIMENSIONS = [
     (56, 48, 40, 32),
 ]
 
-BASIS_NAMES = [
-    "pca--centered",
-    "prca--centered",
-    "prca-abs--centered",
-]
+# BASIS_NAMES = [
+#     "pca--centered",
+#     "prca--centered",
+#     "prca-abs--centered",
+# ]
 
-LAMBDA_LAYER_FOR_POLICIES = {
-    "nothing": {"default": 0},
-    "basis-identity:pca--uncentered": {
-        "resnet18xscifarcompr1": 1000.0,
-        "resnet18xscifarcompr2": 1000.0,
-        "resnet18xscifarcompr4": 1000.0,
-        "resnet18dims32-24-24-5": 1000.0,
-        "resnet18dims24-16-16-5": 1000.0,
-        "resnet18dims16-8-8-5": 1000.0,
-        "resnet18dims64-48-48-10": 1,
-        "resnet18dims48-32-32-10": 1,
-        "resnet18dims32-16-16-10": 1,
-    },
-    "basis-identity:prca-sortabs--uncentered": {
-        "resnet18xscifarcompr1": 1000.0,
-        "resnet18xscifarcompr2": 1000.0,
-        "resnet18xscifarcompr4": 1000.0,
-        "resnet18dims32-24-24-5": 1000.0,
-        "resnet18dims24-16-16-5": 1000.0,
-        "resnet18dims16-8-8-5": 1000.0,
-        "resnet18dims64-48-48-10": 1,
-        "resnet18dims48-32-32-10": 1,
-        "resnet18dims32-16-16-10": 1,
-    },
-    "basis-identity:random--uncentered": {
-        "resnet18xscifarcompr1": 1000.0,
-        "resnet18xscifarcompr2": 1000.0,
-        "resnet18xscifarcompr4": 1,
-        "resnet18dims32-24-24-5": 1000.0,
-        "resnet18dims24-16-16-5": 1000.0,
-        "resnet18dims16-8-8-5": 1000.0,
-        "resnet18dims64-48-48-10": 1,
-        "resnet18dims48-32-32-10": 1,
-        "resnet18dims32-16-16-10": 1,
-    },
-    "vid": {
-        "resnet18xscifarcompr1": 1000000.0,
-        "resnet18xscifarcompr2": 1000000.0,
-        "resnet18xscifarcompr4": 100000.0,
-        "resnet18dims32-24-24-5": 1000000.0,
-        "resnet18dims24-16-16-5": 1e6,
-        "resnet18dims16-8-8-5": 1e6,
-        "resnet18dims64-48-48-10": 1,
-        "resnet18dims48-32-32-10": 1,
-        "resnet18dims32-16-16-10": 1,
-    },
-    "attention-transfer": {
-        "resnet18xscifarcompr1": 1000.0,
-        "resnet18xscifarcompr2": 10.0,
-        "resnet18xscifarcompr4": 10.0,
-    },
-    "fitnet": {
-        "resnet18xscifarcompr1": 0.01,
-        "resnet18xscifarcompr2": 0.001,
-        "resnet18xscifarcompr4": 0.001,
-    },
-}
+# LAMBDA_LAYER_FOR_POLICIES = {
+#     "nothing": {"default": 0},
+#     "basis-identity:pca--uncentered": {
+#         "resnet18xscifarcompr1": 1000.0,
+#         "resnet18xscifarcompr2": 1000.0,
+#         "resnet18xscifarcompr4": 1000.0,
+#         "resnet18dims32-24-24-5": 1000.0,
+#         "resnet18dims24-16-16-5": 1000.0,
+#         "resnet18dims16-8-8-5": 1000.0,
+#         "resnet18dims64-48-48-10": 1,
+#         "resnet18dims48-32-32-10": 1,
+#         "resnet18dims32-16-16-10": 1,
+#     },
+#     "basis-identity:prca-sortabs--uncentered": {
+#         "resnet18xscifarcompr1": 1000.0,
+#         "resnet18xscifarcompr2": 1000.0,
+#         "resnet18xscifarcompr4": 1000.0,
+#         "resnet18dims32-24-24-5": 1000.0,
+#         "resnet18dims24-16-16-5": 1000.0,
+#         "resnet18dims16-8-8-5": 1000.0,
+#         "resnet18dims64-48-48-10": 1,
+#         "resnet18dims48-32-32-10": 1,
+#         "resnet18dims32-16-16-10": 1,
+#     },
+#     "basis-identity:random--uncentered": {
+#         "resnet18xscifarcompr1": 1000.0,
+#         "resnet18xscifarcompr2": 1000.0,
+#         "resnet18xscifarcompr4": 1,
+#         "resnet18dims32-24-24-5": 1000.0,
+#         "resnet18dims24-16-16-5": 1000.0,
+#         "resnet18dims16-8-8-5": 1000.0,
+#         "resnet18dims64-48-48-10": 1,
+#         "resnet18dims48-32-32-10": 1,
+#         "resnet18dims32-16-16-10": 1,
+#     },
+#     "vid": {
+#         "resnet18xscifarcompr1": 1000000.0,
+#         "resnet18xscifarcompr2": 1000000.0,
+#         "resnet18xscifarcompr4": 100000.0,
+#         "resnet18dims32-24-24-5": 1000000.0,
+#         "resnet18dims24-16-16-5": 1e6,
+#         "resnet18dims16-8-8-5": 1e6,
+#         "resnet18dims64-48-48-10": 1,
+#         "resnet18dims48-32-32-10": 1,
+#         "resnet18dims32-16-16-10": 1,
+#     },
+#     "attention-transfer": {
+#         "resnet18xscifarcompr1": 1000.0,
+#         "resnet18xscifarcompr2": 10.0,
+#         "resnet18xscifarcompr4": 10.0,
+#     },
+#     "fitnet": {
+#         "resnet18xscifarcompr1": 0.01,
+#         "resnet18xscifarcompr2": 0.001,
+#         "resnet18xscifarcompr4": 0.001,
+#     },
+# }
 
 
-def get_lamba_layer_for_policy_student(policy: str, student: str) -> float:
-    policy_lambda_layers: dict = LAMBDA_LAYER_FOR_POLICIES[policy]
+# def get_lamba_layer_for_policy_student(policy: str, student: str) -> float:
+#     policy_lambda_layers: dict = LAMBDA_LAYER_FOR_POLICIES[policy]
 
-    key = student if student in policy_lambda_layers else "default"
+#     key = student if student in policy_lambda_layers else "default"
 
-    lambda_layer = policy_lambda_layers[key]
+#     lambda_layer = policy_lambda_layers[key]
 
-    print(f"Retrieving `lambda_layer`(policy=`{policy}`, key=`{key}`)={lambda_layer}")
+#     print(f"Retrieving `lambda_layer`(policy=`{policy}`, key=`{key}`)={lambda_layer}")
 
-    return lambda_layer
+#     return lambda_layer
 
 
 ARCH_LAYER_DIMENSIONS = dict(
@@ -155,5 +155,11 @@ ARCH_LAYER_DIMENSIONS = dict(
         "features.26": 256,
         "features.39": 512,
         "features.52": 512,
+    },
+    nfnetf0={
+        "stages.0": 256,
+        "stages.1": 512,
+        "stages.2": 1536,
+        "stages.3": 1536,
     },
 )
--- xaikd/models/__init__.py
+++ xaikd/models/__init__.py
@@ -101,7 +101,7 @@ def get_layer_output_dimensions(model: nn.Module, layer: str) -> int:
     return getattr(model, "__layer_dimension")[layer]
 
 
-from . import resnet, vgg
+from . import resnet, vgg, nfnet
 
 
 def split_model_at_layer(
