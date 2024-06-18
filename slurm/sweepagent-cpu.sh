3b0c84fc9d08f13e85c5fa4f29bd9342a435b34e
PattarawatChormai <pat.chormai@gmail.com>
2024-06-18 21:55:02 +0200

    add slurm/sweepagent-cpu.sh

---
+++ slurm/sweepagent-cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --mem=32GB
+#SBATCH -o ./logs/array/%A_%a.out
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=p.chormai@tu-berlin.de
+
+nvidia-smi
+
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+export WANDB__SERVICE_WAIT=300
+
+#poetry run wandb agent "$@"
+#WITH_DATA=1 ./runpy wandb agent "$@"
+./runpy wandb agent "$@"
