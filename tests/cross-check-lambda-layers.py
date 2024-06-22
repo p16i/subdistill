5ea0b5a69e786b7588cce2c95f10fb4b0ec3d2d3
Pat Chormai <pat.chormai@gmail.com>
2024-06-22 13:41:30 +0200

    add test case

--- tests/cross-check-lambda-layers.py
+++ tests/cross-check-lambda-layers.py
@@ -18,6 +18,10 @@ from xaikd import constants
         ),
         ("imagenet-vgg16-tv", "imagenet-clean", (0, 1, 1, 0, 1, 1)),
         ("imagenet-vgg16-tv", "imagenet-small", (5, 2, 2, 5, 1, 2)),
+        ("imagenet-nfnetf0-dm", "imagenet-clean", (1, 2, 1, 3, 5, 2)),
+        ("imagenet-nfnetf0-dm", "imagenet-small", (2, 2, 4, 4, 5, 2)),
+        ("imagenet-vitb-tv", "imagenet-clean", (0, 3, 3, 2, None, 1)),
+        ("imagenet-vitb-tv", "imagenet-small", (0, 4, 3, 2, None, 2)),
     ],
 )
 @pytest.mark.parametrize(
@@ -25,7 +29,7 @@ from xaikd import constants
     list(
         enumerate(
             [
-                "fitnet-relu",
+                "fitnet",
                 "attention-transfer",
                 "vid",
                 "basis-identity:pca--uncentered",
@@ -36,6 +40,14 @@ from xaikd import constants
     ),
 )
 def test(teacher, config, expected, pix, policy):
+    if teacher == "imagenet-vitb-tv" and "prca" in policy:
+        pytest.skip("unsupported configuration")
+
+    if policy == "fitnet":
+        if teacher in ["imagenet-nfnetf0-dm", "imagenet-vitb-tv"]:
+            policy = "fitnet-noact"
+        else:
+            policy = "fitnet-relu"
 
     np.testing.assert_equal(
         constants.DEFAULT_LAMBDA_LAYER[config][teacher][policy],
