d816a176278d3c78aec18d66f342ae3b46cc1272
Pat Chormai <pat.chormai@gmail.com>
2024-06-28 16:23:29 +0200

    add jpeg artifact for all samples

--- xaikd/datasets/imagenet.py
+++ xaikd/datasets/imagenet.py
@@ -181,6 +181,8 @@ class TorchVisionDatasetImageNetWithSpuriousFeature(tvd.ImageNet):
 
         assert self.transform is not None
 
+        sample = spurious_feature_generator.jpeg_artifact(sample)
+
         if index in self.victim_indices:
             sample = self.modify_sample(sample)
 
