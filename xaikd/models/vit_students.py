d0b19ec144aeefe89b86105830dbb0d5a5f597d7
Pat Chormai <pat.chormai@gmail.com>
2024-10-05 11:21:18 -0700

    add vitstudent 6 layers

--- xaikd/models/vit_students.py
+++ xaikd/models/vit_students.py
@@ -16,11 +16,11 @@ from . import MODEL_GENERATORS
 from . import vit
 
 
-def _generate_vit_student(num_classes: int, hidden_dim: int, **kwargs):
+def _generate_vit_student(num_layers: int, num_classes: int, hidden_dim: int, **kwargs):
     # ref: https://github.com/pytorch/vision/blob/main/torchvision/models/vision_transformer.py#L621
     model = _vision_transformer(
         patch_size=16,
-        num_layers=4,
+        num_layers=num_layers,
         num_heads=12,
         hidden_dim=hidden_dim,
         mlp_dim=3072 // 4,
@@ -31,8 +31,6 @@ def _generate_vit_student(num_classes: int, hidden_dim: int, **kwargs):
 
     vit.make_encoder_intermediate_output_have_cnn_like_shape_(model)
 
-    # ?todo: add adapter module in each of these block
-
     return model
 
 
@@ -41,7 +39,11 @@ def _generate_model_function():
     for hidden_dim in constants.ARR_VIT_STUDENT_HIDDEN_DIMENSIONS:
 
         MODEL_GENERATORS[f"vitstudent-{hidden_dim}"] = partial(
-            _generate_vit_student, hidden_dim=hidden_dim
+            _generate_vit_student, hidden_dim=hidden_dim, num_layers=4
+        )
+
+        MODEL_GENERATORS[f"vitstudent6l-{hidden_dim}"] = partial(
+            _generate_vit_student, hidden_dim=hidden_dim, num_layers=6
         )
 
 
