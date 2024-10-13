c0fb92baec283413aabf71aa39ff8004d00ed1ca
Pat Chormai <pat.chormai@gmail.com>
2024-10-12 17:49:39 -0700

    bump version

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 
 {
   languages.python.enable = true;
-  languages.python.version = "3.11.10";
+  languages.python.version = "3.11.8";
   languages.python.poetry.enable = true;
   languages.python.poetry.activate.enable = true;
 }
