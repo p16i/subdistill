22122f488b57cefbc3d5f590328b265db987769f
Pat Chormai <pat.chormai@gmail.com>
2024-10-15 22:29:45 -0700

    update new mobilenet

--- xaikd/models/students_mobilenet.py
+++ xaikd/models/students_mobilenet.py
@@ -82,6 +82,62 @@ def _student_very_small(num_classes, dim1, dim2, dim3, **kwargs) -> nn.Module:
     )
 
 
+def _student_very_small_bottleneck(
+    num_classes, dim1, dim2, dim3, dim4, **kwargs
+) -> nn.Module:
+    dilation = 1
+
+    width_mult = 1
+    bneck_conf = partial(mobilenetv3.InvertedResidualConfig, width_mult=width_mult)
+    adjust_channels = partial(
+        mobilenetv3.InvertedResidualConfig.adjust_channels, width_mult=width_mult
+    )
+
+    inverted_residual_setting = [
+        # same conf as mobiletnet-s
+        bneck_conf(16, 3, 16, 16, True, "RE", 2, 1),  # C1
+        bneck_conf(16, 3, 72, 24, False, "RE", 2, 1),  # C2
+        bneck_conf(24, 3, 88, 24, False, "RE", 1, 1),
+        bneck_conf(24, 5, 96, 40, True, "HS", 2, 1),  # C3
+        bneck_conf(40, 5, 240, 40, True, "HS", 1, 1),
+        bneck_conf(40, 5, 240, 40, True, "HS", 1, 1),
+        # modified conf
+        bneck_conf(40, 5, 120, dim1, True, "HS", 1, 1),
+        bneck_conf(dim1, 5, dim2, dim1, True, "HS", 1, 1),
+        bneck_conf(dim1, 5, dim2 * 2, dim1 * 2, True, "HS", 2, dilation),  # C4
+        bneck_conf(
+            dim1 * 2,
+            5,
+            dim3,
+            dim1 * 2,
+            True,
+            "HS",
+            1,
+            dilation,
+        ),
+        bneck_conf(
+            dim1 * 2,
+            5,
+            dim3,
+            dim1 * 2,
+            True,
+            "HS",
+            1,
+            dilation,
+        ),
+    ]
+    last_channel = adjust_channels(dim4)  # C5
+
+    return mobilenetv3._mobilenet_v3(
+        inverted_residual_setting,
+        last_channel,
+        num_classes=num_classes,
+        weights=None,
+        progress=False,
+        **kwargs,
+    )
+
+
 def _student_very_small_cifar(num_classes, dim1, dim2, dim3, **kwargs) -> nn.Module:
     dilation = 1
 
@@ -193,6 +249,9 @@ def _student_very_small_cifarv2(num_classes, dim1, dim2, dim3, **kwargs) -> nn.M
 def _generate_model_function():
 
     MODEL_GENERATORS[f"student-mobilenets"] = _student_s
+    MODEL_GENERATORS[f"student-bmobilenets"] = partial(
+        _student_very_small_bottleneck, dim1=48, dim2=72, dim3=64, dim4=24
+    )
 
     MODEL_GENERATORS[f"student-mobilenetxs"] = partial(
         _student_very_small, dim1=24, dim2=144, dim3=128
