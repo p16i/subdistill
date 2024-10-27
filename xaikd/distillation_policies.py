42c1a2407a10d0cd9c50882718bdc8b28238f8b2
Pat Chormai <pat.chormai@gmail.com>
2024-10-26 17:44:20 -0700

    add santiy check

--- xaikd/distillation_policies.py
+++ xaikd/distillation_policies.py
@@ -742,6 +742,8 @@ class Attn(nn.Module):
         alpha = alpha.unsqueeze(1)
         assert alpha.shape == (B, 1, h, w)
 
+        np.testing.assert_allclose(torch.flatten(alpha, start_dim=1).sum(dim=1), 1.0)
+
         scaled_x = alpha * x
         return scaled_x
 
