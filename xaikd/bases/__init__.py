b1a84418671fdafbc0b9dedec4415b6280419da3
Pat Chormai <pat.chormai@gmail.com>
2024-12-23 06:53:06 +0100

    fix typo

--- xaikd/bases/__init__.py
+++ xaikd/bases/__init__.py
@@ -21,7 +21,7 @@ from . import pcalookahead
 from enum import Enum
 
 from xaikd.bases import pcalookahead
-from xaikd.bases.learners import PRCAGreedyLeaner, PRCARecon
+from xaikd.bases.learners import PRCAGreedyLearner, PRCARecon
 from xaikd import models
 
 
