9c880642a93559d4061c324ab6867674ec92f9b5
Pat Chormai <pat.chormai@gmail.com>
2024-12-30 13:34:56 +0100

    add prca-sign-align-greedy

--- xaikd/bases/learners.py
+++ xaikd/bases/learners.py
@@ -229,15 +229,13 @@ class PRCAGreedyLearner:
         return loss + reg
 
 
-class PRCARecon:
-
+class PRCAReconGreedy:
     def fit(
         self,
         activation: npt.NDArray,
         context: npt.NDArray,
         epochs=100,
         seed=1,
-        eps=1e-6,  # todo: this parameter seem to be very important!
         largest_k=None,
         device="cpu",
     ) -> npt.NDArray:
@@ -282,14 +280,18 @@ class PRCARecon:
 
             v = v.to(device)
 
+            # take only activation and context that are not captured by the learned directions
+            activation = activation @ U_complement
+            context = context @ U_complement
+
             for _ in range(epochs):
                 v.requires_grad_(True)
                 v.grad = None
 
-                obj = self._obj_func(activation, context, U_complement, v)
+                obj = self._obj_func(activation, context, v)
 
                 tbar.set_description_str(
-                    f"PRCA Recon [device={device}] obj={obj.detach().cpu().numpy():.4e}"
+                    f"PRCAReconGreedy [device={device}] obj={obj.detach().cpu().numpy():.4e}"
                 )
 
                 obj.backward()
@@ -303,7 +305,6 @@ class PRCARecon:
                     @torch.no_grad()
                     def linesearch(p, g):
                         # todo: separate this from function
-                        # fast version
                         max_alpha = 2 * np.pi / torch.linalg.norm(g)
 
                         arr_steps = torch.linspace(0, max_alpha, 100).to(device)
@@ -317,9 +318,7 @@ class PRCARecon:
 
                         arr_directions = term_cos + term_sin
 
-                        ref_rel = (
-                            (activation @ U_complement) * (context @ U_complement)
-                        ).sum(axis=1, keepdims=True)
+                        ref_rel = (activation * context).sum(axis=1, keepdims=True)
 
                         rel_on_dir = (activation @ arr_directions) * (
                             context @ arr_directions
@@ -382,11 +381,8 @@ class PRCARecon:
     def _obj_func(
         activation: torch.Tensor,
         context: torch.Tensor,
-        IUUt: torch.Tensor,
         u: torch.Tensor,
     ) -> torch.Tensor:
-        activation = activation @ IUUt
-        context = context @ IUUt
 
         activation_projected = activation.matmul(u)
         context_projected = context.matmul(u)
@@ -403,5 +399,191 @@ class PRCARecon:
 
         # convert the problem into maximization problem.
         loss = -obj.mean()
+
+        return loss
+
+
+class PRCASignAlignGreedy:
+    def fit(
+        self,
+        activation: npt.NDArray,
+        context: npt.NDArray,
+        epochs=100,
+        seed=1,
+        largest_k=None,
+        device="cpu",
+    ) -> npt.NDArray:
+        assert activation.shape == context.shape
+
+        _, d = activation.shape
+
+        activation = activation / ((np.mean(activation**2) ** (1 / 2)) * (d ** (1 / 4)))
+        context = context / ((np.mean(context**2) ** (1 / 2)) * (d ** (1 / 4)))
+
+        activation = torch.from_numpy(activation).float().to(device)
+        context = torch.from_numpy(context).float().to(device)
+
+        ori_activation = activation.clone()
+        ori_context = context.clone()
+
+        ori_rel = (ori_activation * ori_context).sum(dim=1)
+
+        rng = torch.Generator()
+        rng.manual_seed(seed)
+
+        U = torch.zeros(d, d)
+        U = U.to(device)
+
+        I = torch.eye(d).to(device)
+
+        if largest_k is None:
+            largest_k = d
+
+        tbar = tqdm(
+            range(largest_k),
+            total=largest_k,
+            desc=f"PRCASignAlignGreedy [device={device}]]",
+        )
+
+        for k in tbar:
+            if k >= 1:
+                Uk = U[:, :k]
+                U_pinv = torch.linalg.pinv(Uk)
+                U_complement = I - U_pinv.T @ Uk.T
+            else:
+                U_pinv = 0
+                U_complement = I
+
+            v = torch.randn(d, generator=rng).to(device)
+            v = v / torch.linalg.norm(v)
+
+            v = v.to(device)
+
+            # take only activation and context that are not captured by the learned directions
+            activation = activation @ U_complement
+            context = context @ U_complement
+
+            for _ in range(epochs):
+                v.requires_grad_(True)
+                v.grad = None
+
+                obj = self._obj_func(activation, context, v)
+
+                obj.backward()
+
+                with torch.no_grad():
+                    ov = v
+
+                    grad = v.grad
+                    grad = (I - torch.outer(v, v)) @ grad
+
+                    @torch.no_grad()
+                    def linesearch(p, g):
+                        max_alpha = 2 * np.pi / torch.linalg.norm(g)
+
+                        arr_steps = torch.linspace(0, max_alpha, 100).to(device)
+                        norm_g = torch.linalg.norm(g)
+
+                        # construct candidate from exponential map at different time steps
+                        term_cos = torch.outer(p, torch.cos(norm_g * arr_steps))
+                        term_sin = torch.outer(
+                            (g / norm_g), torch.sin(norm_g * arr_steps)
+                        )
+
+                        arr_directions = term_cos + term_sin
+
+                        ref_rel = (activation * context).sum(axis=1, keepdims=True)
+
+                        rel_on_dir = (activation @ arr_directions) * (
+                            context @ arr_directions
+                        )
+
+                        arr_obj_directions = (
+                            torch.mean(ref_rel * rel_on_dir, axis=0)
+                            .detach()
+                            .cpu()
+                            .numpy()
+                        )
+
+                        assert arr_obj_directions.shape == (
+                            arr_steps.shape[0],
+                        ), arr_obj_directions.shape
+
+                        best_ix = np.argmax(arr_obj_directions)
+                        best_lr = arr_steps[best_ix].detach().cpu()
+
+                        return best_lr
+
+                    if torch.linalg.norm(grad).detach().cpu().numpy() == 0:
+                        break
+
+                    lr = linesearch(v, grad)
+
+                    assert lr >= 0
+
+                    if lr == 0:
+                        break
+
+                    # update v with gradient `ascent`.
+                    v = exponential_map(v, grad, lr)
+
+                    # theorethically, the exponential map should return a vector with unitnorm,
+                    # but sometimes there is some numerical instability.
+                    v = v / torch.linalg.norm(v)
+
+                    np.testing.assert_allclose(
+                        torch.linalg.norm(v).detach().cpu(), 1.0, atol=1e-3
+                    )
+
+                    if (v @ ov).abs() > (1 - eps):
+                        # stop if the solution isn't update anymore.
+                        break
+
+            U[:, k] = v.detach()
+
+            with torch.no_grad():
+
+                Uk = U[:, : (k + 1)]
+
+                projected_rel = ((ori_activation @ Uk) * (ori_context @ Uk)).sum(dim=1)
+                assert projected_rel.shape == ori_rel.shape
+
+                sign_align = (torch.sign(ori_rel) * torch.sign(projected_rel)).mean()
+
+                perc_sign_align = sign_align.detach().cpu().numpy()
+                tbar.set_description_str(
+                    f"DevPRCASignAlign [device={device}] perc_sign_align={perc_sign_align:.4f}"
+                )
+
+        with torch.no_grad():
+            Q, R = torch.linalg.qr(U)
+            U = Q
+        np.testing.assert_allclose(
+            (U.T @ U).detach().cpu().numpy(), np.eye(d), atol=1e-6
+        )
+
+        return U.detach().cpu().numpy()
+
+    @staticmethod
+    def _obj_func(
+        activation: torch.Tensor,
+        context: torch.Tensor,
+        u: torch.Tensor,
+    ) -> torch.Tensor:
+
+        activation_projected = activation.matmul(u)
+        context_projected = context.matmul(u)
+
+        assert len(activation_projected.shape) == len(context_projected.shape) == 1
+
+        relevance_original = (activation * context).sum(dim=1)
+        relevance_projected = activation_projected * context_projected
+        assert relevance_original.shape == relevance_projected.shape
+
+        obj = relevance_original * relevance_projected
+
+        assert len(obj.shape) == 1 and obj.shape[0] == activation.shape[0]
+
+        loss = obj.mean()
 
         return loss
