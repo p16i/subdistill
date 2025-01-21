a0fcc5ca4f4d08708e8d72da0b64430cfc6c0c46
Pat Chormai <pat.chormai@gmail.com>
2025-01-21 11:01:46 +0100

    relax assertion

--- xaikd/utils/ndarray_sampling.py
+++ xaikd/utils/ndarray_sampling.py
@@ -6,7 +6,9 @@ from numpy import typing as npt
 def get_random_permutations(
     rng: np.random.Generator, n: int, total: int, num_chosen: int
 ):
-    assert num_chosen <= total
+
+    if num_chosen > total:
+        print(f"[warning] num_chosen ({num_chosen}) > total ({total}); effectively, we only permutate locations.")
 
     candidates = np.arange(total)
 
