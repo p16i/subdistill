14b68aa16f0874fc9264211da8efd68c40649669
Pat Chormai <pat.chormai@gmail.com>
2025-02-13 07:35:06 +0100

    fix import after refactor

--- xaikd/utils/__init__.py
+++ xaikd/utils/__init__.py
@@ -209,6 +209,7 @@ def modify_last_layer_for_subclasses(
 def get_dimensions_at_layers(
     model: nn.Module, dataloader: DataLoader, layers: typing.List[str], device="cpu"
 ) -> typing.Dict[str, int]:
+    # todo: this should be part of interceptor
     assert not model.training
 
     hooks = []
