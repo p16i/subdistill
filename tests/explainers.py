1234bf06dce0c456e72d7f4a939c2862cfb0c69f
Pat Chormai <pat.chormai@gmail.com>
2025-02-24 11:20:31 +0100

    refactor

--- tests/explainers.py
+++ tests/explainers.py
@@ -30,7 +30,7 @@ def _check_heatmap_finite(explainer_name: str, model_name: str):
     dl = DataLoader(ds)
 
     arr_logits, arr_heatmaps = explainer.explain(
-        dl, logit_modifiers.MultiClassTargetLogit(num_classes=1000), device=device
+        dl, logit_modifiers.MultiClassTargetLogit(), device=device
     )
 
     assert np.isfinite(arr_logits).all()
