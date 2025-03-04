d4ac40e84f822da87f70c2a5002794abe5d3c5c8
Pat Chormai <pat.chormai@gmail.com>
2025-03-04 10:16:43 +0100

    update sweepagent

--- slurm/sweepagent-cpu.sh
+++ slurm/sweepagent-cpu.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#SBATCH --mem=32GB
+#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=12
 #SBATCH -o ./logs/array/%A_%a.out
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=p.chormai@tu-berlin.de
-
-nvidia-smi
 
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 export WANDB__SERVICE_WAIT=300
 
-#poetry run wandb agent "$@"
-#WITH_DATA=1 ./runpy wandb agent "$@"
 ./runpy wandb agent "$@"
