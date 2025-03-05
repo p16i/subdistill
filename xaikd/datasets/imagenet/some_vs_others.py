3bf0cefb421f33138253815968c707000ed69468
Pat Chormai <pat.chormai@gmail.com>
2025-03-05 15:09:16 +0100

    fix after refactor

--- xaikd/datasets/imagenet/some_vs_others.py
+++ xaikd/datasets/imagenet/some_vs_others.py
@@ -6,31 +6,35 @@ from torch.utils.data import Dataset
 
 from ..register import add_dataset_to_registry
 from . import IMAGENET_SUPERCLASS_MAPPING
-from .original import ImageNet
+from .original import ImageNetBase
 
 
-class ImageNetSuperclassVsOthers(ImageNet):
+class ImageNetSuperclassVsOthers(ImageNetBase):
+
     def __init__(self, super_class: str):
         super().__init__()
 
-        self.selected_classes = IMAGENET_SUPERCLASS_MAPPING[super_class]
-        self.num_classes = 1
+        self._superclass = super_class
+        self._selected_classes = IMAGENET_SUPERCLASS_MAPPING[super_class]
 
-    def transform_target(self, target: int) -> int:
-        if target in self.selected_classes:
-            return 1
-        else:
-            return 0
+    @property
+    def target_transform(self):
+        def transform(target):
 
-    def create_subset(
-        self,
-        train_split=False,
-        target_transform: typing.Union[None, typing.Callable] = None,
-    ) -> Dataset:
+            if target in self.selected_classes:
+                return 1
+            else:
+                return 0
 
-        return super().create_subset(
-            train_split=train_split, target_transform=self.transform_target
-        )
+        return transform
+
+    @property
+    def selected_classes(self) -> typing.List[int]:
+        return self._selected_classes
+
+    @property
+    def num_classes(self) -> int:
+        return 1
 
 
 def register_datasets():
