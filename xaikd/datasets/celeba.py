f762d6109884feed1599be72a14e6c28e0795377
Pat Chormai <pat.chormai@gmail.com>
2025-03-05 15:16:17 +0100

    datasets: refactor celeba

--- xaikd/datasets/celeba.py
+++ xaikd/datasets/celeba.py
@@ -21,26 +21,40 @@ from .register import register_dataset, add_dataset_to_registry
 from . import DATADIR, DatasetConfiguration
 
 
-CELEBA_NUM_ATTRIBUTES = 40
+NUM_CELEBA_ATTRIBUTES = 40
 
 DEFAULT_TRANSFORMATION = ResNet18_Weights.IMAGENET1K_V1.transforms()
 
 
-@register_dataset("celeba")
-class CelebA(DatasetConfiguration):
+class CelebABase(DatasetConfiguration):
+
     def __init__(self):
-        self._normalizer = transforms.Normalize(
-            # ref: https://github.com/pytorch/vision/blob/main/torchvision/transforms/_presets.py#L44
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
+
+        np.testing.assert_allclose(
+            self.input_transformation.mean, self._normalizer.mean
         )
 
-        # ref: https://github.com/pytorch/vision/blob/main/torchvision/transforms/_presets.py#L38
-        self.input_transformation = DEFAULT_TRANSFORMATION
+    def create_subset(
+        self,
+        train_split=False,
+        target_transform: typing.Union[None, typing.Callable] = None,
+    ) -> Dataset:
+        return self.dataclass(
+            root=str(DATADIR),
+            split="train" if train_split else "valid",
+            transform=self.input_transformation,
+            target_transform=self.target_transform,
+        )
 
+    @property
+    def input_transformation(self):
+        # ref: https://github.com/pytorch/vision/blob/main/torchvision/transforms/_presets.py#L38
+        return DEFAULT_TRANSFORMATION
+
+    @property
+    def input_training_transformation(self):
         # ref: https://github.com/pytorch/examples/blob/main/imagenet/main.py#L238
-        # todo: when do we use this?
-        self.input_training_transformation = transforms.Compose(
+        return transforms.Compose(
             [
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
@@ -49,30 +63,37 @@ class CelebA(DatasetConfiguration):
             ]
         )
 
-        np.testing.assert_allclose(
-            self.input_transformation.mean, self._normalizer.mean
+    @property
+    def _normalizer(self) -> transforms.Normalize:
+
+        # ref: https://github.com/pytorch/vision/blob/main/torchvision/transforms/_presets.py#L44
+        return transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
         )
 
-        self.dataclass = tvd.CelebA
-        self.root = DATADIR
-
-    def create_subset(
-        self,
-        train_split=False,
-        target_transform: typing.Union[None, typing.Callable] = None,
-    ) -> Dataset:
-        return self.dataclass(
-            root=self.root,
-            split="train" if train_split else "valid",
-            transform=self.input_transformation,
-            target_transform=target_transform,
-        )
-
-    def transform_target(self, target: torch.Tensor) -> torch.Tensor:
-        return target
+    @property
+    def dataclass(self):
+        return tvd.CelebA
 
 
-class CelebAAttribute(CelebA):
+@register_dataset("celeba")
+class CelebA(CelebABase):
+
+    @property
+    def selected_classes(self) -> typing.List[int]:
+        return list(range(NUM_CELEBA_ATTRIBUTES))
+
+    @property
+    def num_classes(self) -> int:
+        return NUM_CELEBA_ATTRIBUTES
+
+    @property
+    def target_transform(self):
+        return None
+
+
+class CelebAAttribute(CelebABase):
     def __init__(self, attr_ix: int):
         super().__init__()
 
@@ -80,31 +101,28 @@ class CelebAAttribute(CelebA):
 
         self.attr_ix = attr_ix
 
-    def create_subset(
-        self,
-        train_split=False,
-        target_transform: typing.Union[None, typing.Callable] = None,
-    ) -> Dataset:
-        if target_transform is not None:
-            print("Warning: setting target_transform has no effect here!")
+    @property
+    def selected_classes(self) -> typing.List[int]:
+        return [self.attr_ix]
 
-        return self.dataclass(
-            root=self.root,
-            split="train" if train_split else "valid",
-            transform=self.input_transformation,
-            target_transform=self.transform_target,
-        )
+    @property
+    def num_classes(self) -> int:
+        return 1
 
-    def transform_target(self, target: torch.Tensor) -> torch.Tensor:
+    @property
+    def target_transform(self):
+        def transform(target):
 
-        assert len(target.shape) == 1
-        assert len(target) > 1
+            assert len(target.shape) == 1
+            assert len(target) > 1
 
-        return target[self.attr_ix]
+            return target[self.attr_ix]
+
+        return transform
 
 
 def _register_celeba_attributes():
-    for attr_ix in range(CELEBA_NUM_ATTRIBUTES):
+    for attr_ix in range(NUM_CELEBA_ATTRIBUTES):
         add_dataset_to_registry(
             f"celeba-attr{attr_ix}", partial(CelebAAttribute, attr_ix=attr_ix)
         )
