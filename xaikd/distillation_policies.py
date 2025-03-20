696f4395d63f77a85779ea53b85e1624c9268d43
Pat Chormai <pat.chormai@gmail.com>
2025-03-20 14:43:29 +0100

    refine policies

--- xaikd/distillation_policies.py
+++ xaikd/distillation_policies.py
@@ -827,6 +827,8 @@ class OrthogonalBasisIdentityBiasPolicy(LayerPolicy):
         ) / (w * h)
         loss_mse = loss_mse.flatten(start_dim=1)
 
+        loss_mse = loss_mse / self.basis.get_scale_factors_for_k(k).max()
+
         # sum over all spatial dimensions
         loss_mse = loss_mse.sum(dim=1)
 
@@ -962,46 +964,7 @@ class OrthogonalBasisIdentityBatchNormPolicy(LayerPolicy):
 
 
 @register_layer_policy("basis-bn-no-scale")
-class OrthogonalBasisIdentityBatchNormPolicy(LayerPolicy):
-    def __init__(
-        self, teacher_dims: int, student_dims: int, device: str, basis: OrthogonalBasis
-    ) -> None:
-        super().__init__()
-
-        k = student_dims
-
-        self.basis = basis
-        self.transformer_teacher_feats = basis.construct_adapter(
-            k=k, mode=AdapterMode.ENCODER, device=device
-        )
-
-        self.transformer_student_feats = nn.BatchNorm2d(
-            num_features=k, track_running_stats=True, affine=True
-        ).to(device)
-
-    def criterion(self, transformed_teacher_feats, transformed_student_feats):
-        b, k, w, h = transformed_teacher_feats.shape
-
-        assert transformed_teacher_feats.shape == transformed_student_feats.shape
-
-        loss_mse = F.mse_loss(
-            transformed_student_feats, transformed_teacher_feats, reduction="none"
-        ) / (w * h)
-        loss_mse = loss_mse.flatten(start_dim=1)
-
-        # sum over all spatial dimensions
-        loss_mse = loss_mse.sum(dim=1)
-
-        assert loss_mse.shape == (b,)
-
-        # average over all samples
-        loss_mse = loss_mse.mean()
-
-        return loss_mse
-
-
-@register_layer_policy("basis-bn-wo-normalization")
-class OrthogonalBasisIdentityPolicy(LayerPolicy):
+class OrthogonalBasisIdentityBatchNormNoScalePolicy(LayerPolicy):
     def __init__(
         self, teacher_dims: int, student_dims: int, device: str, basis: OrthogonalBasis
     ) -> None:
