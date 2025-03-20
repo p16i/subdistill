fc7c6d2e1db9cb622a3508c676084c6d5ad3ccd5
Pat Chormai <pat.chormai@gmail.com>
2025-03-20 16:57:46 +0100

    policy: add basis-bn-only-mean-no-scale

--- xaikd/utils/modules.py
+++ xaikd/utils/modules.py
@@ -221,3 +221,66 @@ def load_model_from_wandb_artifact(
         )
         model.eval()
         return model
+
+
+# todo: add test
+class BatchNormOnlyMean(batchnorm._BatchNorm):
+    # ref: https://github.com/pytorch/pytorch/blob/main/torch/nn/modules/batchnorm.py#L193
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        self._check_input_dim(input)
+
+        # exponential_average_factor is set to self.momentum
+        # (when it is available) only so that it gets updated
+        # in ONNX graph when this node is exported to ONNX.
+        if self.momentum is None:
+            exponential_average_factor = 0.0
+        else:
+            exponential_average_factor = self.momentum
+
+        if self.training and self.track_running_stats:
+            # TODO: if statement only here to tell the jit to skip emitting this when it is None
+            if self.num_batches_tracked is not None:  # type: ignore[has-type]
+                self.num_batches_tracked.add_(1)  # type: ignore[has-type]
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
+
+        r"""
+        Decide whether the mini-batch stats should be used for normalization rather than the buffers.
+        Mini-batch stats are used in training mode, and in eval mode when buffers are None.
+        """
+        if self.training:
+            bn_training = True
+        else:
+            bn_training = (self.running_mean is None) and (self.running_var is None)
+
+        r"""
+        Buffers are only updated if they are to be tracked and we are in training mode. Thus they only need to be
+        passed when the update should occur (i.e. in training mode when they are tracked), or when buffer stats are
+        used for normalization (i.e. in eval mode when buffers are not None).
+        """
+
+        assert self.running_mean is not None
+        unit_variance = torch.ones_like(self.running_mean).to(self.running_mean.device)
+
+        return F.batch_norm(
+            input,
+            # If buffers are not to be tracked, ensure that they won't be updated
+            (
+                self.running_mean
+                if not self.training or self.track_running_stats
+                else None
+            ),
+            unit_variance,
+            self.weight,
+            self.bias,
+            bn_training,
+            exponential_average_factor,
+            self.eps,
+        )
+
+    # ref: https://github.com/pytorch/pytorch/blob/main/torch/nn/modules/batchnorm.py#L450
+    def _check_input_dim(self, input):
+        if input.dim() != 4:
+            raise ValueError(f"expected 4D input (got {input.dim()}D input)")
