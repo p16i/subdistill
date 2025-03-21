8aaca14d0250b081dfa8109c16a209cca6b80d09
Pat Chormai <pat.chormai@gmail.com>
2025-03-21 14:54:40 +0100

    fix tests

--- tests/bases.py
+++ tests/bases.py
@@ -310,11 +310,10 @@ def test_correct_scale_orthogoal_bases(basis_name):
     )
 
     U = basis.U
-    scale = basis.scale_factors
+    actual = basis.scale_factors
 
-    np.testing.assert_allclose(
-        scale, [np.mean((utils.flatten_3d_tensor(arr_act) @ U[:, 0]) ** 2)]
-    )
+    expected = np.mean((utils.flatten_3d_tensor(arr_act) @ U) ** 2, axis=0)
+    np.testing.assert_allclose(actual, expected)
 
 
 @pytest.mark.parametrize(
