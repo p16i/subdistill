710f3b86ac03380be3d0b100834d96118ecd5679
Pat Chormai <pat.chormai@gmail.com>
2025-03-22 10:58:52 +0100

    fix weight

--- scripts/train-celeba-teacher.py
+++ scripts/train-celeba-teacher.py
@@ -104,16 +104,24 @@ def model_generator(arch: str) -> typing.Optional[nn.Module]:
     num_outputs = datasets.celeba.NUM_CELEBA_ATTRIBUTES
     model = None
     if arch == "resnet18":
-        model = torchvision.models.resnet18(pretrained=True)
+        model = torchvision.models.resnet18(
+            weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1
+        )
         model.fc = nn.Linear(model.fc.in_features, num_outputs)
     elif arch == "resnet50":
-        model = torchvision.models.resnet50(pretrained=True)
+        model = torchvision.models.resnet50(
+            weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2
+        )
         model.fc = nn.Linear(model.fc.in_features, num_outputs)
     elif arch == "vitb16":
-        model = torchvision.models.vit_b_16(pretrained=True)
+        model = torchvision.models.vit_b_16(
+            weights=torchvision.models.ViT_B_16_Weights.IMAGENET1K_V1
+        )
         model.heads.head = nn.Linear(model.hidden_dim, num_outputs)
     elif arch == "wideresnet50-2":
-        model = torchvision.models.wide_resnet50_2(pretrained=True)
+        model = torchvision.models.wide_resnet50_2(
+            weights=torchvision.models.WideResNet50_2_Weights.IMAGENET1K_V1
+        )
         model.fc = nn.Linear(model.fc.in_features, num_outputs)
     else:
         raise ValueError(f"{arch} doesn't exist")
