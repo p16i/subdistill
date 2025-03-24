e57541ebbce5f19e507e6a3863f0b4742c68d40b
Pat Chormai <pat.chormai@gmail.com>
2025-03-24 16:16:19 +0100

    add centering

--- xaikd/bases/__init__.py
+++ xaikd/bases/__init__.py
@@ -14,7 +14,7 @@ from .helpers import learn_basis
 # todo: do we still need this?
 def _add_centering_variants():
 
-    for base_variant_cls in [PCA, PRCAPosDef]:
+    for base_variant_cls in [PCA, PRCAPosDef, PRCAPosDefWeightSTDWithP1]:
         base_variant_slug = base_variant_cls.slug()
         slug = f"{base_variant_slug}--centered"
 
