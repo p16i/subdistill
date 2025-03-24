f0e2c953e41dfeac4fb1653b35e8534397376e82
Pat Chormai <pat.chormai@gmail.com>
2025-03-24 14:17:36 +0100

    fix scaling

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -336,7 +336,7 @@ class OrthogonalBasisCenteringAndScalePolicy(LayerPolicy):
         ) / (w * h)
         loss_mse = loss_mse.flatten(start_dim=1)
 
-        loss_mse = loss_mse / self.basis.get_scale_factors_for_k(k).max()
+        loss_mse = loss_mse / self.basis.get_scale_factors_for_k(k).sum()
 
         # sum over all spatial dimensions
         loss_mse = loss_mse.sum(dim=1)
