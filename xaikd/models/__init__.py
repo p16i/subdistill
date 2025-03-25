bd4528db546cec7dd9b9a90aef2474ffd94528f7
Pat Chormai <pat.chormai@gmail.com>
2025-03-25 17:58:42 +0100

    update

--- xaikd/models/__init__.py
+++ xaikd/models/__init__.py
@@ -35,12 +35,19 @@ MODEL_CHECKPOINT_MAPPING = {
 }
 
 
+def add_model_to_registry(name: str, fn: Callable):
+    assert name not in MODEL_GENERATORS
+
+    MODEL_GENERATORS[name] = fn
+
+
 def register_model(name):
     """Decorator to register a data modality provider."""
 
     def wrapped(fn):
         """Wrapped function to register a data modality provider with name `name`"""
-        MODEL_GENERATORS[name] = fn
+
+        add_model_to_registry(name, fn)
 
         return fn
 
