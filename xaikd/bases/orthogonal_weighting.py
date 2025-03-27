caa2894d73f034f7d3c960369d4ede7e7fa4fd39
Pat Chormai <pat.chormai@gmail.com>
2025-03-27 08:56:11 +0100

    revert revising

--- xaikd/bases/orthogonal_weighting.py
+++ xaikd/bases/orthogonal_weighting.py
@@ -74,19 +74,35 @@ class PRCAPosDefWeightSTDFromEntropy(PRCAPosDef):
         self, arr_act, arr_ctx, mean_act, arr_logodd, logodd_threshold, **kwargs
     ):
 
+        arr_act = utils.flatten_3d_tensor(arr_act)
+        N, _ = arr_act.shape
+
         weights = self._compute_sample_weight(
             arr_logodd=arr_logodd, logodd_threshold=logodd_threshold
         )
         arr_ctx = arr_ctx * weights.reshape((-1, 1, 1))
 
-        return super()._solve(
-            arr_act=arr_act,
-            arr_ctx=arr_ctx,
-            mean_act=mean_act,
-            arr_logodd=arr_logodd,
-            logodd_threshold=logodd_threshold,
-            **kwargs,
+        arr_ctx = utils.flatten_3d_tensor(arr_ctx)
+        mean_ctx = np.mean(arr_ctx, axis=0)
+
+        cov_a = (arr_act.T @ arr_act) / N - np.outer(mean_act, mean_act)
+        tr_cov_a = np.trace(cov_a)
+
+        cov_c = (arr_ctx.T @ arr_ctx) / N
+        tr_cov_c = np.trace(cov_c)
+
+        cov_ac = (arr_act.T @ arr_ctx) / N - np.outer(mean_act, mean_ctx)
+        cov_acca = cov_ac + cov_ac.T
+
+        cov = (
+            (1 / np.sqrt(tr_cov_a * tr_cov_c)) * cov_acca
+            + (2 / tr_cov_a) * cov_a
+            + (2 / tr_cov_c) * cov_c
         )
+
+        _, eigvecs = utils.solve_eigh(cov)
+
+        return eigvecs
 
     @classmethod
     def slug(cls):
