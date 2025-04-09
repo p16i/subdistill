0b1047470738b0e5e254173b8e276c4b36ad5e67
Pat Chormai <pat.chormai@gmail.com>
2025-04-09 14:27:53 +0200

    student: efficientformerv2

---
+++ xaikd/models/efficientformer.py
@@ -0,0 +1,17 @@
+import pytest
+
+import timm
+from torch import nn
+
+from . import add_model_to_registry
+
+
+def _construct_effcientformerv2s0(num_classes: int) -> nn.Module:
+    return timm.create_model("efficientformerv2_s0", num_classes=num_classes)
+
+
+def _add_model_to_register():
+    add_model_to_registry("student-efficientformerv2_s0", _construct_effcientformerv2s0)
+
+
+_add_model_to_register()
