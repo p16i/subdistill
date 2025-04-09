8fa1140e61a845dcb92f42ae84639dc3d5a5e93a
Pat Chormai <pat.chormai@gmail.com>
2025-04-09 14:42:05 +0200

    make signature of efftransformer constructor compatible

--- xaikd/models/efficientformer.py
+++ xaikd/models/efficientformer.py
@@ -6,7 +6,7 @@ from torch import nn
 from . import add_model_to_registry
 
 
-def _construct_effcientformerv2s0(num_classes: int) -> nn.Module:
+def _construct_effcientformerv2s0(num_classes: int, **kwargs) -> nn.Module:
     return timm.create_model("efficientformerv2_s0", num_classes=num_classes)
 
 
