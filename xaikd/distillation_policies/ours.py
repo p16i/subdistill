ea54d772e32f635a3953d09d03854b2e20dedfb1
Pat Chormai <pat.chormai@gmail.com>
2025-04-10 12:53:09 +0200

    investigate normalization

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -151,3 +151,97 @@ class OrthogonalBasisBatchNormSumNormalizedLearnablePolicy(
             self.transformer_teacher_feats.mean = nn.Parameter(
                 self.transformer_teacher_feats.mean
             )
+
+
+@register_policy("basis-bn-max-normalized")
+class OrthogonalBasisBatchNormMaxNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self, teacher_dims: int, student_dims: int, device: str, basis: OrthogonalBasis
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.scaling_factor = np.max(self.basis.get_scale_factors_for_k(student_dims))
+
+        print(
+            f"basis-bn (teacher_dim={teacher_dims}); scaling factor={self.scaling_factor} "
+        )
+
+        self.transformer_student_feats = nn.BatchNorm2d(
+            num_features=k, track_running_stats=True, affine=True
+        ).to(device)
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats, transformed_teacher_feats, reduction="none"
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        loss_mse = loss_mse / self.scaling_factor
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
+
+
+@register_policy("basis-bn-no-normalized")
+class OrthogonalBasisBatchNormMaxNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self, teacher_dims: int, student_dims: int, device: str, basis: OrthogonalBasis
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.scaling_factor = 1
+
+        print(
+            f"basis-bn (teacher_dim={teacher_dims}); scaling factor={self.scaling_factor} "
+        )
+
+        self.transformer_student_feats = nn.BatchNorm2d(
+            num_features=k, track_running_stats=True, affine=True
+        ).to(device)
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats, transformed_teacher_feats, reduction="none"
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        loss_mse = loss_mse / self.scaling_factor
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
