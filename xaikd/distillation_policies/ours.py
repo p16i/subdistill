1e15db654628e311b1389df554ff1ba4782a12cd
Pat Chormai <pat.chormai@gmail.com>
2025-04-11 09:09:05 +0200

    try scaling only

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -236,3 +236,57 @@ class OrthogonalBasisBatchNormNoNormalizedPolicy(LayerPolicy):
         loss_mse = loss_mse.mean()
 
         return loss_mse
+
+
+@register_policy("basis-bn-scale")
+class OrthogonalBasisBatchNormSumNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self, teacher_dims: int, student_dims: int, device: str, basis: OrthogonalBasis
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        class StudentTranformer(nn.Module):
+            def __init__(self, k: int, arr_scaling_factors: npt.NDArray):
+                super().__init__()
+                self.bn = nn.BatchNorm2d(
+                    num_features=k, track_running_stats=True, affine=False
+                ).to(device)
+                self.scaling = (
+                    torch.from_numpy(arr_scaling_factors**0.5)
+                    .reshape((1, -1, 1, 1))
+                    .to(device)
+                )
+
+            def forward(self, x: torch.Tensor):
+                return self.scaling * self.bn(x)
+
+        self.transformer_student_feats = StudentTranformer(
+            k=k, arr_scaling_factors=self.basis.get_scale_factors_for_k(student_dims)
+        )
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats, transformed_teacher_feats, reduction="none"
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
