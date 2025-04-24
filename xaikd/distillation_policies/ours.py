d97f5d25736ab1ad011b879869d2fc454185e784
Pat Chormai <pat.chormai@gmail.com>
2025-04-24 19:32:11 +0200

    add more basis

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -196,14 +196,6 @@ class OrthogonalBasisCenterSumNormalizedPolicy(LayerPolicy):
             k=k, mode=AdapterMode.ENCODER, device=device
         )
 
-        self.scaling_factor = (
-            np.max(self.basis.get_scale_factors_for_k(student_dims)) ** 0.5
-        )
-
-        print(
-            f"basis-bn (teacher_dim={teacher_dims}); scaling factor={self.scaling_factor} "
-        )
-
         self.transformer_student_feats = utils.modules.Centering2D(num_features=k).to(
             device
         )
@@ -220,7 +212,113 @@ class OrthogonalBasisCenterSumNormalizedPolicy(LayerPolicy):
         ) / (w * h)
         loss_mse = loss_mse.flatten(start_dim=1)
 
-        loss_mse = loss_mse / self.scaling_factor
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
+
+
+@register_policy("basis-center--teacher-feat-normalized")
+class OrthogonalBasisCenterSumNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.scaling_factor = (
+            np.max(self.basis.get_scale_factors_for_k(student_dims)) ** 0.5
+        )
+
+        self.transformer_student_feats = utils.modules.Centering2D(num_features=k).to(
+            device
+        )
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats,
+            transformed_teacher_feats / self.scaling_factor,
+            reduction="none",
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
+
+
+class CenteringWithScaling(nn.Module):
+    # fixme: add test
+    def __init__(self, k: int, init_scale: float):
+        super().__init__()
+
+        self.scale = torch.nn.Parameter(torch.tensor(init_scale))
+        self.centering = utils.modules.Centering2D(num_features=k)
+
+    def forward(self, feat: torch.Tensor):
+        return self.scale * self.centering(feat)
+
+
+@register_policy("basis-center--with-scaling")
+class OrthogonalBasisCenterSumNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.transformer_student_feats = CenteringWithScaling(k=k, init_scale=1.0).to(
+            device
+        )
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats,
+            transformed_teacher_feats,
+            reduction="none",
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
 
         # sum over all spatial dimensions
         loss_mse = loss_mse.sum(dim=1)
@@ -302,6 +400,106 @@ class OrthogonalBasisCenterRotationPolicy(LayerPolicy):
         loss_mse = F.mse_loss(
             transformed_student_feats,
             transformed_teacher_feats,
+            reduction="none",
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
+
+
+@register_policy("basis-center-rotation--scale-one-init")
+class OrthogonalBasisCenterRotationPolicy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        init_scale = 1.0
+
+        self.transformer_student_feats = nn.Sequential(
+            utils.modules.Centering2D(num_features=k).to(device),
+            BasisOrthoStudenTransform(k=k, init_scale=init_scale),
+        ).to(device)
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats,
+            transformed_teacher_feats,
+            reduction="none",
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
+
+
+@register_policy("basis-center-rotation--teacher-feat-and-normalized-scale-one-init")
+class OrthogonalBasisCenterRotationTeacherFeatNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.normalization_coeff = float(
+            np.max(self.basis.get_scale_factors_for_k(student_dims)) ** 0.5
+        )
+
+        self.transformer_student_feats = nn.Sequential(
+            utils.modules.Centering2D(num_features=k).to(device),
+            BasisOrthoStudenTransform(k=k, init_scale=1.0),
+        ).to(device)
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats,
+            transformed_teacher_feats / self.normalization_coeff,
             reduction="none",
         ) / (w * h)
         loss_mse = loss_mse.flatten(start_dim=1)
