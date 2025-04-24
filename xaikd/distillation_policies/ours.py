8a5d781f40cbf00f4f39f49c08c42702160ccca9
Pat Chormai <pat.chormai@gmail.com>
2025-04-24 15:31:35 +0200

    add basis-center

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -123,3 +123,61 @@ class OrthogonalBasisBatchNormSumNormalizedLearnablePolicy(
             self.transformer_teacher_feats.mean = nn.Parameter(
                 self.transformer_teacher_feats.mean
             )
+
+
+@register_policy("basis-center")
+class OrthogonalBasisCenterSumNormalizedPolicy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        if layerwise_training:
+            # fixme: add tests
+            self.scaling_factor = 1
+        else:
+            self.scaling_factor = np.sum(
+                self.basis.get_scale_factors_for_k(student_dims)
+            )
+
+        print(
+            f"basis-bn (teacher_dim={teacher_dims}); scaling factor={self.scaling_factor} "
+        )
+
+        self.transformer_student_feats = utils.modules.Centering2D(num_features=k).to(
+            device
+        )
+
+    def criterion(self, transformed_teacher_feats, transformed_student_feats):
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats, transformed_teacher_feats, reduction="none"
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        loss_mse = loss_mse / self.scaling_factor
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+
+        return loss_mse
