75d1ffa9496937c517c07e18ff69ff3839956976
Pat Chormai <pat.chormai@gmail.com>
2025-04-30 23:21:46 +0200

    fix test

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -92,97 +92,6 @@ class OrthogonalBasisBatchNormSumNormalizedPolicy(LayerPolicy):
         return loss_mse
 
 
-class RotationTransform(nn.Module):
-    # fixme: add test
-    def __init__(self, k: int):
-        super().__init__()
-
-        self.rotation = nn.utils.parametrizations.orthogonal(
-            nn.Linear(in_features=k, out_features=k, bias=False)
-        )
-
-    def forward(self, feat: torch.Tensor):
-
-        b, d, h, w = feat.shape
-
-        feat = feat.reshape((b, d, h * w))
-
-        feat = feat.permute(1, 0, 2)
-        feat = feat.flatten(start_dim=1)
-        # shape: [b*h*w, d]
-        feat = feat.T
-        feat = self.rotation(feat)
-
-        # reshape back
-        # shape: [d, b*h*w]
-        feat = feat.T
-        feat = feat.reshape(d, b, h * w)
-
-        feat = feat.permute(1, 0, 2)
-        feat = feat.reshape((b, d, h, w))
-
-        return feat
-
-
-@register_policy("basis-bn-rotation-sum-normalized")
-class OrthogonalBasisBatchNormSumNormalizedPolicy(LayerPolicy):
-    def __init__(
-        self,
-        teacher_dims: int,
-        student_dims: int,
-        device: str,
-        basis: OrthogonalBasis,
-        layerwise_training: bool,
-    ) -> None:
-        super().__init__()
-
-        k = student_dims
-
-        self.basis = basis
-        self.transformer_teacher_feats = basis.construct_adapter(
-            k=k, mode=AdapterMode.ENCODER, device=device
-        )
-
-        if layerwise_training:
-            # fixme: add tests
-            self.scaling_factor = 1
-        else:
-            self.scaling_factor = np.sum(
-                self.basis.get_scale_factors_for_k(student_dims)
-            )
-
-        print(
-            f"basis-bn (teacher_dim={teacher_dims}); scaling factor={self.scaling_factor} "
-        )
-
-        self.transformer_student_feats = nn.Sequential(
-            nn.BatchNorm2d(num_features=k, track_running_stats=True, affine=True),
-            RotationTransform(k=k),
-        ).to(device)
-
-    def criterion(self, transformed_teacher_feats, transformed_student_feats):
-        b, k, w, h = transformed_teacher_feats.shape
-
-        assert transformed_teacher_feats.shape == transformed_student_feats.shape
-
-        loss_mse = F.mse_loss(
-            transformed_student_feats, transformed_teacher_feats, reduction="none"
-        ) / (w * h)
-        loss_mse = loss_mse.flatten(start_dim=1)
-
-        loss_mse = loss_mse / self.scaling_factor
-
-        # sum over all spatial dimensions
-        loss_mse = loss_mse.sum(dim=1)
-
-        assert loss_mse.shape == (b,)
-
-        # average over all samples
-        loss_mse = loss_mse.mean()
-
-        return loss_mse
-
-
 @register_policy("basis-center-rotation")
 class OrthogonalBasisCenterRotationPolicy(LayerPolicy):
 
@@ -200,7 +109,6 @@ class OrthogonalBasisCenterRotationPolicy(LayerPolicy):
 
         self.basis = basis
 
-        # fixme: add tests
         if layerwise_training:
             self.scaling_factor = 1
         else:
@@ -213,7 +121,7 @@ class OrthogonalBasisCenterRotationPolicy(LayerPolicy):
         )
 
         self.transformer_student_feats = nn.Sequential(
-            utils.modules.Centering2D(num_features=k).to(device),
+            utils.modules.Centering2D(num_features=k, affine=False).to(device),
             utils.modules.RotateAndScale(k=k),
         ).to(device)
 
