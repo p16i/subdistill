059884dcd3f688d6b8bcc41897bccff51f0658d0
Pat Chormai <pat.chormai@gmail.com>
2025-06-02 17:41:08 +0200

    add new policy

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -293,6 +293,30 @@ class AblationIdentityTeacherCenterLinear(AblationTemplate):
         ).to(device)
 
 
+@register_policy("basis-ablation--center-normalized-teacher--center-linear")
+class AblationIdentityTeacherCenterLinear(AblationTemplate):
+    def _construct_teacher_transformation(
+        self,
+        basis: OrthogonalBasis,
+        k: int,
+        device: str,
+    ):
+        _, d = basis.U.shape
+        return nn.Sequential(
+            # we perform only centering and no projection
+            basis.construct_adapter(k=d, mode=AdapterMode.ENCODER, device=device),
+            Normalization(float(np.sum(basis.get_scale_factors_for_k(d)))),
+        ).to(device)
+
+    def _construct_student_transformation(self, k: int, device: str):
+
+        _, d = self.basis.U.shape
+        return nn.Sequential(
+            utils.modules.Centering2D(num_features=k, affine=False).to(device),
+            nn.Conv2d(in_channels=k, out_channels=d, bias=False, kernel_size=1),
+        ).to(device)
+
+
 @register_policy("basis-ablation--normalized-teacher--center-rotation-scale")
 class AblationNormalizedTeacherCenterRotationScale(AblationTemplate):
     def _construct_teacher_transformation(
