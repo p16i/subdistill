a0f59a4a706f0e45820a9abe4a87d297407440f7
Pat Chormai <pat.chormai@gmail.com>
2025-06-02 09:44:11 +0200

    fix transformation

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -289,7 +289,7 @@ class AblationIdentityTeacherCenterLinear(AblationTemplate):
         _, d = self.basis.U.shape
         return nn.Sequential(
             utils.modules.Centering2D(num_features=k, affine=False).to(device),
-            nn.Linear(in_features=k, out_features=d, bias=False),
+            nn.Conv2d(in_channels=k, out_channels=d, bias=False, kernel_size=1),
         ).to(device)
 
 
