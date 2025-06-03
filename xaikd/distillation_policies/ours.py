e2782250c46999ef3bfa59de44784679795e5dfa
Pat Chormai <pat.chormai@gmail.com>
2025-06-03 12:51:32 +0200

    add policy

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -270,6 +270,24 @@ class AblationNoNormalizedTeacherCenterRotation(AblationTemplate):
         ).to(device)
 
 
+@register_policy("basis-ablation--no-normalized-teacher--center")
+class AblationNoNormalizedTeacherCenter(AblationTemplate):
+    def _construct_teacher_transformation(
+        self,
+        basis: OrthogonalBasis,
+        k: int,
+        device: str,
+    ):
+        return nn.Sequential(
+            basis.construct_adapter(k=k, mode=AdapterMode.ENCODER, device=device),
+        ).to(device)
+
+    def _construct_student_transformation(self, k: int, device: str):
+        return nn.Sequential(
+            utils.modules.Centering2D(num_features=k, affine=False).to(device),
+        ).to(device)
+
+
 @register_policy("basis-ablation--loss-cosine--no-normalized-teacher--center-rotation")
 class AblationLossCosNoNormalizedTeacherCenterRotation(AblationTemplate):
     def _construct_teacher_transformation(
