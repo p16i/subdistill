824f8546c6fb2940ec2abc2c8be9cfa36f120f75
Pat Chormai <pat.chormai@gmail.com>
2025-06-19 14:25:22 +0200

    fix typo

--- xaikd/subspace_analyes.py
+++ xaikd/subspace_analyes.py
@@ -58,7 +58,7 @@ def evaluate_low_rank_approximation(
         )
     )
 
-    for k in tqdm(arr_ks, tqdm="evaluating low-rank approximation"):
+    for k in tqdm(arr_ks, desc="evaluating low-rank approximation"):
 
         pt_Uk = pt_U[:, :k]
         mat_proj = pt_Uk.T @ pt_Uk
