9e0e408d75e8a354138b223948ed2e1e190f5efd
Pat Chormai <pat.chormai@gmail.com>
2025-07-22 23:03:32 +0200

    log quantiles

--- xaikd/distillators/trainer_wrapper.py
+++ xaikd/distillators/trainer_wrapper.py
@@ -277,16 +277,16 @@ class LayerwiseKDModelWrapper(pl.LightningModule):
                 f"grass_dist_{layer_name}",
                 dist,
             )
-            # self.log(f"max_theta_{layer_name}", torch.max(thetas))
-            # self.log(f"median_theta_{layer_name}", torch.median(thetas))
-            # self.log(f"min_theta_{layer_name}", torch.min(thetas))
-            # self.log(f"p99_theta_{layer_name}", torch.quantile(thetas, 99))
-            # self.log(f"p90_theta_{layer_name}", torch.quantile(thetas, 90))
+            self.log(f"max_theta_{layer_name}", torch.max(thetas))
+            self.log(f"median_theta_{layer_name}", torch.median(thetas))
+            self.log(f"min_theta_{layer_name}", torch.min(thetas))
+            self.log(f"p99_theta_{layer_name}", torch.quantile(thetas, 0.99))
+            self.log(f"p90_theta_{layer_name}", torch.quantile(thetas, 0.90))
 
-            self.logger.experiment.log(
-                {"theta": wandb.Histogram(thetas.cpu().numpy())},
-                step=self.current_epoch,
-            )
+            # self.logger.experiment.log(
+            #     {"theta": wandb.Histogram(thetas.cpu().numpy())},
+            #     step=self.current_epoch,
+            # )
 
 
 def dist_grassmainain(U1, U2):
