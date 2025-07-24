47f42ed434742de5ee81a07b927359b3cec96d32
Pat Chormai <pat.chormai@gmail.com>
2025-07-24 15:01:20 +0200

    skip for now

--- xaikd/attributors.py
+++ xaikd/attributors.py
@@ -224,8 +224,9 @@ def extract_activation_grad(
 
     arr_ctx = np.vstack(arr_ctx)
 
-    arr_logit = np.array(arr_logit)
-    assert len(arr_logit.shape) == 1, f"arr_logit.shape={arr_logit.shape}"
+    arr_logit = None
+    # arr_logit = np.array(arr_logit)
+    # assert len(arr_logit.shape) == 1, f"arr_logit.shape={arr_logit.shape}"
 
     # assert arr_act.shape[0] == arr_logit.shape[0]
     assert arr_act.shape == arr_ctx.shape
