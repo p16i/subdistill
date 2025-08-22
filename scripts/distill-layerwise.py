7fee83dbce4dfd7744418818d38d8fc1f9106e0a
Pat Chormai <pat.chormai@gmail.com>
2025-08-22 11:11:29 +0200

    fix variable name

--- scripts/distill-layerwise.py
+++ scripts/distill-layerwise.py
@@ -266,8 +266,8 @@ def main(
         new_bias = b_teacher + W_teacher @ mean
 
         task_ix = int(dataset_name.split("attr")[-1])
-        student.fc.weight.data = new_weight.data[task_ix, :]
-        student.fc.bias.data = new_bias.data[task_ix]
+        student_model.fc.weight.data = new_weight.data[task_ix, :]
+        student_model.fc.bias.data = new_bias.data[task_ix]
 
         utils.freeze_model(student.fc)
 
