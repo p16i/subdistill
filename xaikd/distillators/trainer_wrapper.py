fix scaling factor call

xaikd/distillators/trainer_wrapper.py
@@ -138,7 +138,7 @@ class LayerwiseKDModelWrapper(pl.LightningModule):
 
         assert torch.isfinite(loss_layer)
 
-        global_scaling_factor = self.layer_policy_collection.global_scaling_factor
+        global_scaling_factor = self.layer_policy_collection.global_scaling_factor()
 
         return loss_layer * global_scaling_factor
 