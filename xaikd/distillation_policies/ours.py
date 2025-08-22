5fd74af5d6b69c6fc70aa24c5ae25a9ee9949ba9
Pat Chormai <pat.chormai@gmail.com>
2025-08-22 18:12:59 +0200

    add policy with smaller batchnorm mm

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -209,6 +209,67 @@ class OrthogonalBasisCenterRotationV2Policy(LayerPolicy):
         return loss_mse
 
 
+@register_policy("basis-center-rotationv2mt0.01")
+class OrthogonalBasisCenterRotationV2Policy(LayerPolicy):
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__()
+
+        k = student_dims
+
+        self.basis = basis
+
+        if layerwise_training:
+            self.scaling_factor = 1
+        else:
+            self.scaling_factor = np.sum(
+                self.basis.get_scale_factors_for_k(student_dims)
+            )
+
+        self.transformer_teacher_feats = basis.construct_adapter(
+            k=k, mode=AdapterMode.ENCODER, device=device
+        )
+
+        self.transformer_student_feats = nn.Sequential(
+            utils.modules.Centering2D(num_features=k, affine=False, momentum=0.01).to(
+                device
+            ),
+            utils.modules.Rotate(k=k),
+        ).to(device)
+
+    def criterion(
+        self, transformed_teacher_feats, transformed_student_feats
+    ) -> torch.Tensor:
+        b, k, w, h = transformed_teacher_feats.shape
+
+        assert transformed_teacher_feats.shape == transformed_student_feats.shape
+
+        loss_mse = F.mse_loss(
+            transformed_student_feats,
+            transformed_teacher_feats,
+            reduction="none",
+        ) / (w * h)
+        loss_mse = loss_mse.flatten(start_dim=1)
+
+        loss_mse = loss_mse / self.scaling_factor
+
+        # sum over all spatial dimensions
+        loss_mse = loss_mse.sum(dim=1)
+
+        assert loss_mse.shape == (b,)
+
+        # average over all samples
+        loss_mse = loss_mse.mean()
+        
+        return loss
+
+
 @register_policy("basis-center-rotation-binxent")
 class OrthogonalBasisCenterRotationV2Policy(LayerPolicy):
     def __init__(
