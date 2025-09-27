d215b3103d5543b5df045582dd15fdbe6ac5920c
Pat Chormai <pat.chormai@gmail.com>
2025-09-27 09:45:25 +0200

    reverse init

--- xaikd/models/resnet.py
+++ xaikd/models/resnet.py
@@ -262,12 +262,12 @@ def construct_student_cifar_resnet18_varying_dims(
 
     model.fc = nn.Linear(model.inplanes, num_classes)
 
-    for m in model.modules():
-        if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
+    # for m in model.modules():
+    #     if isinstance(m, nn.Conv2d):
+    #         nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+    #     elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+    #         nn.init.constant_(m.weight, 1)
+    #         nn.init.constant_(m.bias, 0)
 
     # model.relu = nn.ELU()
 
