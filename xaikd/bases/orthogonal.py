4b100f613b706f108888e6c260fce73b9e76529d
Pat Chormai <pat.chormai@gmail.com>
2025-10-27 20:46:40 +0100

    basis: prcaposdefv2

--- xaikd/bases/orthogonal.py
+++ xaikd/bases/orthogonal.py
@@ -51,7 +51,6 @@ class Identity(OrthogonalBasis):
 @register_basis()
 class Random(OrthogonalBasis):
     def _solve(self, arr_act, arr_ctx, arr_logodd, logodd_threshold, **kwargs):
-
         seed = kwargs["seed"]
 
         arr_act = utils.flatten_3d_tensor(arr_act)
@@ -67,7 +66,6 @@ class Random(OrthogonalBasis):
 @register_basis()
 class GradPCA(OrthogonalBasis):
     def _solve(self, arr_act, arr_ctx, arr_logodd, logodd_threshold, **kwargs):
-
         arr_ctx = utils.flatten_3d_tensor(arr_ctx)
         N, _ = arr_ctx.shape
 
@@ -129,6 +127,55 @@ class PRCAPosDef(OrthogonalBasis):
         coef_acca = 1
         coef_a = 2 * np.sqrt(tr_c / tr_a)
         coef_c = 2 * np.sqrt(tr_a / tr_c)
+
+        print(
+            f"Coefficients: coeff_acca={coef_acca:.4e}, coeff_a={coef_a:.4e}, coeff_c={coef_c:.4e} "
+            + f"tr_a={tr_a:.4e}, tr_c={tr_c:.4e}"
+        )
+
+        cov_pos_def = coef_acca * cov_acca + coef_a * cov_a + coef_c * cov_c
+
+        eigvals, eigvecs = utils.solve_eigh(cov_pos_def)
+
+        min_eigval = np.min(eigvals)
+        max_eigval = np.max(eigvals)
+        print(f"range(eigvals)=[{min_eigval:.4e}, {max_eigval:.4e}]")
+
+        # Mathematically, the eigenvalues should be non-negative.
+        # Due to numerical stablity, there are situations that we have negative values.
+        # In such cases, we tolerate and raise an error if the smallest value is relatetive large.
+        if min_eigval < 0 and (np.abs(min_eigval) / max_eigval) > 1e-16:
+            # fixme: this need to be handle properly
+            print(
+                f"[warning] We have {np.sum(eigvals < 0)} negative eigvals (the smallest one is {min_eigval:.4e}) (largest one is {max_eigval:.4e})"
+            )
+            # raise ValueError(
+            #     f"We have {np.sum(eigvals < 0)} negative eigvals (the smallest one is {min_eigval:.4e}) (largest one is {max_eigval:.4e})"
+            # )
+
+        return eigvecs
+
+
+@register_basis()
+class PRCAPosDefv2(OrthogonalBasis):
+    def _solve(self, arr_act, arr_ctx, arr_logodd, logodd_threshold, **kwargs):
+        arr_act = utils.flatten_3d_tensor(arr_act)
+        N, _ = arr_act.shape
+
+        arr_ctx = utils.flatten_3d_tensor(arr_ctx)
+
+        cov_a = (arr_act.T @ arr_act) / N
+        tr_a = np.trace(cov_a)
+
+        cov_c = (arr_ctx.T @ arr_ctx) / N
+        tr_c = np.trace(cov_c)
+
+        cov_ac = (arr_act.T @ arr_ctx) / N
+        cov_acca = cov_ac + cov_ac.T
+
+        coef_acca = 1
+        coef_a = np.sqrt(tr_c / tr_a)
+        coef_c = np.sqrt(tr_a / tr_c)
 
         print(
             f"Coefficients: coeff_acca={coef_acca:.4e}, coeff_a={coef_a:.4e}, coeff_c={coef_c:.4e} "
