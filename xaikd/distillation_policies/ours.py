be702ac38cabfe49ef6b729996ab22493b06d3a2
Pat Chormai <pat.chormai@gmail.com>
2025-12-13 10:32:08 +0100

    add frobenius softortho

--- xaikd/distillation_policies/ours.py
+++ xaikd/distillation_policies/ours.py
@@ -323,8 +323,63 @@ class OrthogonalBasisCenterSoftOrthoPolicy(OrthogonalBasisCenterOrthoPolicy):
 
         module.log(f"{prefix}_softortho_sigvals_max", sigvals.max().item())
         module.log(f"{prefix}_softortho_sigvals_min", sigvals.min().item())
-        # this is a stable way to implement ||W^T @ W - I_K ||2^2 
+        # this is a stable way to implement ||W^T @ W - I_K ||2^2
         loss = torch.sum((sigvals - 1.0) ** 2)
+
+        return 1000 * loss
+
+
+@register_policy("basis-center-frobenius-softortho")
+class OrthogonalBasisCenterFrobeniusSoftOrthoPolicy(OrthogonalBasisCenterOrthoPolicy):
+    """
+    This should be use with basis-identity
+    """
+
+    def __init__(
+        self,
+        teacher_dims: int,
+        student_dims: int,
+        device: str,
+        basis: OrthogonalBasis,
+        layerwise_training: bool,
+    ) -> None:
+        super().__init__(teacher_dims, student_dims, device, basis, layerwise_training)
+
+        d = teacher_dims
+        k = student_dims
+
+        lin = nn.Conv2d(in_channels=k, out_channels=d, bias=False, kernel_size=1)
+
+        W = lin.weight.squeeze()  # shape (out_features, in_features)
+
+        U, _, _ = torch.linalg.svd(W, full_matrices=False)
+
+        lin.weight.data = U.unsqueeze(-1).unsqueeze(-1)
+
+        with torch.no_grad():
+            print("Initialized soft-ortho layer with SVD.")
+            svdvals = torch.linalg.svdvals(lin.weight.data.squeeze())
+            print(
+                f"sigvals: min={torch.min(svdvals).item()}, max={torch.max(svdvals).item()}"
+            )
+
+        self.transformer_student_feats = nn.Sequential(
+            utils.modules.Centering2D(num_features=k, affine=False), lin
+        ).to(device)
+
+    def additional_loss(self, module: LightningModule, prefix="") -> torch.Tensor:
+        linear_layer: nn.Linear = self.transformer_student_feats[1]
+        W = linear_layer.weight.squeeze()  # shape (out_features, in_features)
+
+        d, K = W.shape
+
+        assert d >= K
+
+        eye = torch.eye(W.shape[1], device=W.device)
+
+        loss = torch.linalg.norm(W.T @ W - eye, ord="fro") ** 2
+
+        module.log(f"{prefix}_softortho_frobenius", loss.item())
 
         return 1000 * loss
 
